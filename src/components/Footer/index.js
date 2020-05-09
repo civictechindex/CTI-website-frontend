1e8c591ace9759f2e009dc973ec9e0cc6a26fccd
Jeff-Enriquez <jeff.t.hawkes@gmail.com>
2020-05-09 16:07:49 -0700

    Add footer

---
+++ src/components/Footer/index.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import useStyles from './style.js'
+
+const Footer = () => {
+  const classes = useStyles()
+  return (
+    <>
+    <h1>Footer</h1>
+    </>
+  )
+}
+
+export default Footer
---
+++ src/components/Footer/style.js
@@ -0,0 +1,7 @@
+import { createUseStyles } from 'react-jss'
+
+const useStyles = createUseStyles({
+
+})
+
+export default useStyles
