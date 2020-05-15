d7d83d3d8d7472638ff7828e8ef54c4a089b90ca
Jeff-Enriquez <jeff.t.hawkes@gmail.com>
2020-05-15 12:01:21 -0700

    Add home component

---
+++ src/components/Home/index.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import useStyles from './style.js'
+
+const Home = () => {
+  const classes = useStyles()
+
+  return (
+    <>
+    <h1>Home</h1>
+    </>
+  )
+}
+
+export default Home
