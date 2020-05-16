57f7507a5cb379039b9dc60b3630e3c586bc4cc4
Jeff-Enriquez <jeff.t.hawkes@gmail.com>
2020-05-16 16:50:33 -0700

    Create pages directory & move home to pages

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import React from 'react'
 import { Route, Switch } from 'react-router-dom'
 import Header from './components/Header'
 import Footer from './components/Footer'
-import Home from './components/Home'
+import Home from './pages/Home'
 
 const App = () => {
   return (
