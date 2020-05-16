e33239a51109b93aff2bc5564c76caa0bd4f4c3e
Jeff-Enriquez <jeff.t.hawkes@gmail.com>
2020-05-16 16:56:20 -0700

    Create about page

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import { Route, Switch } from 'react-router-dom'
 import Header from './components/Header'
 import Footer from './components/Footer'
 import Home from './pages/Home'
+import About from './pages/About'
 
 const App = () => {
   return (
@@ -10,6 +11,7 @@ const App = () => {
       <Header/>
       <Switch>
         <Route exact path='/' component={Home}/>
+        <Route exact path='/about' component={About}/>
       </Switch>
       <Footer/>
     </>
---
+++ src/pages/About/index.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import useStyles from './styles.js'
+
+const About = () => {
+  const classes = useStyles()
+
+  return (
+    <>
+    <h1>Test</h1>
+    </>
+  )
+}
+
+export default About
