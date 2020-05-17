44f699b11217ed12a1662f012d99f973c2e76baa
Jeff-Enriquez <jeff.t.hawkes@gmail.com>
2020-05-17 02:25:19 -0700

    Style text section

--- src/pages/About/styles.js
+++ src/pages/About/styles.js
@@ -3,21 +3,21 @@ import { createUseStyles } from 'react-jss'
 const useStyles = createUseStyles( theme => ({
   main: {
     fontFamily: theme.primaryFontFamily,
+    color: theme.primaryTextColor,
     '& section':{
       boxSizing: 'border-box',
-      padding: '10px',
+      padding: '0 20px',
     }
   },
-/******     HEADING SECTION      ******/
+/**********     HEADING SECTION      **********/
   headingSection: {
     width: '100%',
-    height: '400px',
+    height: '350px',
     backgroundColor: theme.backgroundColor1,
-    border: '1px solid black'
   },
   headingContainer: {
     width: 'fit-content',
-    margin: '50px 200px 0 200px',
+    margin: '60px 200px 0 200px',
   },
   heading: {
     fontSize: '36px',
@@ -26,18 +26,37 @@ const useStyles = createUseStyles( theme => ({
     maxWidth: '570px',
     marginTop: '60px'
   },
+/**********     TEXT SECTION      **********/
+  textSection: {
+    color: theme.secondaryTextColor,
+    backgroundColor: theme.backgroundColor4,
+    textAlign: 'center',
+  },
+  text: {
+    display: 'inline-block',
+    maxWidth: '770px',
+    fontSize: '26px',
+    lineHeight: '32px',
+    fontWeight: '400',
+    padding: '75px 0',
+  },
 /**********     MEDIA-QUERIES     **********/
   '@media (max-width: 840px)': {
     headingContainer: {
       margin: '50px auto 0 auto'
     },
     heading: {
-      maxWidth: '426px'
+      maxWidth: '426px',
     }
   },
   '@media (max-width: 450px)': {
     heading: {
-      fontSize: '26px'
+      fontSize: '26px',
+      lineHeight: '36px',
+    },
+    text: {
+      fontSize: '20px',
+      lineHeight: '28px',
     }
   }
 }))
