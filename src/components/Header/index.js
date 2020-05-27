efc7c0894360f87eb1af3976a62b673fbfbde443
Jeff-Enriquez <jeff.t.hawkes@gmail.com>
2020-05-26 18:20:37 -0700

    Revise header

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -32,29 +32,28 @@ const Header = () => {
       <Link to='/'><img className={classes.logo} src='/images/cti-logo-bw.svg' alt='civic logo'/></Link>
       <div className={classes.flexContainer}>
         <div className={classes.linksContainer}>
+          <Link to='/' className={classes.links}>Tag Generator</Link>
+        </div>
+        <div className={classes.linksContainer}>
           <Link to='/about' className={classes.links}>About</Link>
           <div className={classes.subLinksContainer}>
-            <Link to='/' className={classes.subLinks}>Overview</Link>
             <Link to='/' className={classes.subLinks}>Searching the Index</Link>
             <Link to='/adding-projects-to-the-index' className={classes.subLinks}>Adding Projects to the Index</Link>
+          </div>
+        </div>
+        <div className={classes.linksContainer}>
+          <Link to='/' className={classes.links}>Contributors</Link>
+          <div className={classes.subLinksContainer}>
             <Link to='/' className={classes.subLinks}>Index Contributors</Link>
           </div>
         </div>
         <div className={classes.linksContainer}>
-          <Link to='/' className={classes.links}>Tag Generator</Link>
-        </div>
-        <div className={classes.linksContainer}>
-          <Link to='/' className={classes.links}>Communicate with Us</Link>
-          <div className={classes.subLinksContainer}>
-            <Link to='/' className={classes.subLinks}>FAQ</Link>
-          </div>
-        </div>
-        <div className={classes.linksContainer}>
-          <Link to='/' className={classes.links}>Support Us</Link>
+          <Link to='/' className={classes.links}>Radical Collaboration</Link>
           <div className={classes.subLinksContainer}>
             <Link to='/' className={classes.subLinks}>Donate</Link>
             <Link to='/' className={classes.subLinks}>Share the CTI</Link>
             <Link to='/' className={classes.subLinks}>Volunteer with Us</Link>
+            <Link to='/' className={classes.subLinks}>FAQ</Link>
           </div>
         </div>
         <div className={classes.searchContainer}>
