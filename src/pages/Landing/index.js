1fb08a99baf3900bf3d88d89e618876a6479de9c
mealthebear <mealthebear@gmail.com>
2020-09-20 03:19:03 -0700

    Implemented coming soon and notification sections

--- src/pages/Landing/index.js
+++ src/pages/Landing/index.js
@@ -6,7 +6,20 @@ const Landing = () => {
 
   return (
     <div>
-      <h2 className={classes.joinHeader}>Join a worldwide movement to catalog every open source civic tech project</h2>
+
+      <section className={classes.headerSection + ' ' + classes.landingSection}>
+        <h2 className={classes.landingHeader}>Join a worldwide movement to catalog every open source civic tech project</h2>
+      </section>
+
+      <section className={classes.comingSoonSection}>
+        <h3 className={classes.comingSoonHeader}>COMING SOON</h3>
+        <div className={classes.notificationContainer}>
+          <h4 className={classes.notificationHeader}>Be the First to Know when the Civic Tech Index launches</h4>
+          <input className={classes.notifyInput} name="email" placeholder="Enter your email address" type="text"></input>
+          <div className={classes.notifyButton}>Notify Me</div>
+        </div>
+      </section>
+
     </div>
   )
 }
--- src/pages/Landing/styles.js
+++ src/pages/Landing/styles.js
@@ -1,11 +1,72 @@
 import { createUseStyles } from 'react-jss'
 
 const useStyles = createUseStyles( theme => ({
-  joinHeader: {
+  comingSoonHeader: {
+    fontFamily: theme.thirdFontFamily,
+    fontSize: '2vw',
+    letterSpacing: '5px',
+  },
+
+  comingSoonSection: {
+    margin: '0 auto',
+    textAlign: 'center',
+    width: '60%',
+  },
+
+  headerSection: {
     color: theme.primaryTextColor,
     fontFamily: theme.primaryFontFamily,
+    margin: '0 auto',
     textAlign: 'center',
-  }
+    width: '60%',
+  },
+
+  landingHeader: {
+    color: 'gold',
+    fontSize: '3vw',
+  },
+
+  notificationContainer: {
+    fontFamily: theme.primaryFontFamily,
+    margin: '0 auto',
+    textAlign: 'center',
+  },
+
+  notificationHeader: {
+    fontFamily: theme.primaryFontFamily,
+    fontSize: '1.8vw',
+    fontWeight: '500',
+    width: '100%',
+  },
+
+  notifyButton: {
+    backgroundColor: '#00b6e3',
+    borderRadius: '0px 25px 25px 0px',
+    color: 'white',
+    cursor: 'pointer',
+    display: 'inline-block',
+    fontSize: '1vw',
+    fontWeight: '500',
+    padding: '15px',
+    width: '15%',
+  },
+
+  notifyInput: {
+    backgroundColor: 'lightgoldenrodyellow',
+    border: 'none',
+    borderRadius: '25px 0px 0px 25px',
+    fontSize: '1vw',
+    padding: '15px',
+    width: '40%',
+    '&:focus': {
+      boxShadow: '1px 1px 10px #333 inset',
+      outline: 'none',
+    },
+    '&::placeholder': {
+      color: 'black',
+      opacity: '1',
+    },
+  },
 }));
 
 export default useStyles;
