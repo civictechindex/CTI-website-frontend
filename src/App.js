02e162d851cf6f877d4a094efae76947e36139ac
SHINergy <jonathan@greenstreetdigital.com>
2020-10-12 23:22:32 -0700

    Adds project search

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import Home from './pages/Home'
 import About from './pages/About'
 import HowToUse from './pages/HowToUse'
 import Landing from './pages/Landing'
+import Projects from './pages/Projects'
 
 const App = () => {
   return (
@@ -13,6 +14,7 @@ const App = () => {
         <Route exact path='/about' component={About}/>
         <Route exact path='/adding-projects-to-the-index' component={HowToUse}/>
         <Route exact path='/landing' component={Landing}/>
+        <Route exact path='/projects' component={Projects}/>
       </Switch>
     </>
   )
