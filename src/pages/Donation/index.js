fa7696a788d5a39c99075367ae6f18fcea4bb436
mealthebear <mealthebear@gmail.com>
2020-10-25 10:59:35 -0700

    Placed donation information and gif on the page

--- src/pages/Donation/index.js
+++ src/pages/Donation/index.js
@@ -14,13 +14,34 @@ const Donation = () => {
   ];
 
   return (
-    <div>
+    <>
       <Header />
-      <section className={classes.titleSection}>
-        <BreadCrumbs crumbs={breadCrumbLinks} />
+
+      <section className={classes.commonSection + ' ' + classes.titleSection}>
+      <BreadCrumbs crumbs={breadCrumbLinks} /> {/* Change left & right padding to 0 */}
         <h2 className={classes.donateTitle}>Make a Donation</h2>
       </section>
-    </div>
+
+      <section className={classes.commonSection + ' ' + classes.infoSection}>
+        <div className={classes.infoText}>
+          <p>The Civic Tech Index is being developed by <span>Hack For LA,</span> and
+            is currently taking donations through <span>Code For America.</span>
+          </p>
+          <p>1. Please follow this link to the Code For America <span>donation form</span></p>
+          <p>2. To make a donation to the Civic Tech Index, please
+            select <strong>Hack For LA</strong> under the brigades listed.
+          </p>
+        </div>
+
+        {/* <div className={classes.infoGif}> */}
+          <img 
+            alt="A gif demonstrating the use of the donation form"
+            className={classes.infoGif}
+            src="./images/CTI-Donate-to-HfLA.gif"
+          />
+        {/* </div> */}
+      </section>
+    </>
   )
 }
 
--- src/pages/Donation/styles.js
+++ src/pages/Donation/styles.js
@@ -1,10 +1,38 @@
 import { createUseStyles } from 'react-jss';
 
 const useStyles = createUseStyles(theme => ({
+  commonSection: {
+    margin: '0 auto',
+    width: '80%',
+  },
+
+  donateCrumbs: {
+    padding: '25px 0px',
+  },
+
   donateTitle: {
     color: '#004364',
     fontFamily: theme.primaryFontFamily,
     fontSize: '3vw',
+  },
+
+  infoGif: {
+    width: '50%',
+  },
+
+  infoText: {
+    boxSizing: 'border-box',
+    display: 'inline-block',
+    fontSize: '1.5vw',
+    padding: '3vw 1vw 0 0',
+    verticalAlign: 'top',
+    width: '50%',
+  },
+
+  titleSection: {
+    '& nav': {
+      padding: '25px 0px !important', // Temporary fix; remove in the future
+    },
   },
 }))
 
