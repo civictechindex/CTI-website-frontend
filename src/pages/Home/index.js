4870cabc330e119adbd1af2951798e9c4eed684b
mealthebear <mealthebear@gmail.com>
2020-11-07 22:05:11 -0800

    Cleaned up code in Home page

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,5 +1,5 @@
-import Header from '../../components/Header'
 import Footer from '../../components/Footer'
+import Header from '../../components/Header'
 import React from 'react'
 import useStyles from './styles.js'
 import '../../styles.css'
@@ -9,72 +9,76 @@ const Home = () => {
 
   return (
     <>
-    <Header />
-    <div className={classes.page}>
-    <section className="page-section world-background">
-      <h1 className={classes.heading}>Join a worldwide movement to catalog every open source civic tech project.</h1>
-      <div className={classes.listContainer}>
-        <div className={classes.listItemContainer}>
-          <p className={classes.listItemText}>Requires no coding</p>
-        </div>
-        <div className={classes.listItemContainer}>
-          <p className={classes.listItemText}>Display a customized index on any site</p>
-        </div>
-        <div className={classes.listItemContainer}>
-          <p className={classes.listItemText}>Index is owned by all who use it</p>
-        </div>
+      <Header />
+      <div className={classes.page}>
+        <section className="page-section world-background">
+          <h1 className={classes.heading}>
+            Join a worldwide movement to catalog every open source civic tech project.
+          </h1>
+          <div className={classes.listContainer}>
+            <div className={classes.listItemContainer}>
+              <p className={classes.listItemText}>Requires no coding</p>
+            </div>
+            <div className={classes.listItemContainer}>
+              <p className={classes.listItemText}>Display a customized index on any site</p>
+            </div>
+            <div className={classes.listItemContainer}>
+              <p className={classes.listItemText}>Index is owned by all who use it</p>
+            </div>
+          </div>
+          <div className={classes.btnContainer}>
+            <button className={`${classes.btn} ${classes.black}`}>Tag Your Project</button>
+            <button className={classes.btn}>Learn More</button>
+          </div>
+        </section>
+        <section className={classes.ctiSection}>
+          <div className={classes.ctiContainer}>
+            <h2 className={classes.ctiHeading}>Civic Tech Index is used by:</h2>
+            <div className={classes.ctiImgsContainer}>
+              <a href='/'>
+                <img className={classes.ctiImgs} src='/images/code-for-all.png' alt='Code for All' />
+              </a>
+              <a href='/'><img className={classes.ctiImgs} src='/images/democracy-lab.svg' alt='DemocracyLab' /></a>
+              <a href='/'><img className={classes.ctiImgs} src='/images/code-for-america.svg' alt='Code for America' /></a>
+              <a href='/'><img className={classes.ctiImgs} src='/images/yale-openlab.svg' alt='Yale OpenLab' /></a>
+              <a href='/'><img className={classes.ctiImgs} src='/images/open-oakland.svg' alt='OpenOakland' /></a>
+              <a href='/'><img className={classes.ctiImgs} src='/images/hack-for-la.png' alt='Hack for LA' /></a>
+              <a href='/'><img className={classes.ctiImgs} src='/images/open-savannah.png' alt='Open Savannah' /></a>
+              <a href='/'><img className={classes.ctiImgs} src='/images/open-san-diego.svg' alt='Open San Diego' /></a>
+            </div>
+          </div>
+        </section>
+        <section className={classes.trendingTopicsSection}>
+          <div className={classes.trendingTopicsContainer}>
+            <h2 className={classes.trendingTopicsHeader}>How are people using the CTI?</h2>
+            <div className={classes.allTopicsContainer}>
+              <div className={classes.topicContainer}>
+                <a href='/'><p className={classes.trendingTopicsDetails}>Covid-19</p></a>
+              </div>
+              <div className={classes.topicContainer}>
+                <a href='/'><p className={classes.trendingTopicsDetails}>Trending Topic 1</p></a>
+              </div>
+              <div className={classes.topicContainer}>
+                <a href='/'><p className={classes.trendingTopicsDetails}>Trending Topic 2</p></a>
+              </div>
+              <div className={classes.topicContainer}>
+                <a href='/'><p className={classes.trendingTopicsDetails}>Trending Topic 3</p></a>
+              </div>
+              <div className={classes.topicContainer}>
+                <a href='/'><p className={classes.trendingTopicsDetails}>Trending Topic 4</p></a>
+              </div>
+              <div className={classes.topicContainer}>
+                <a href='/'><p className={classes.trendingTopicsDetails}>Trending Topic 5</p></a>
+              </div>
+            </div>
+          </div>
+        </section>
+        <section className={classes.getStartedSection}>
+          <h2 className={classes.getStartedHeading}>Ready to get started?</h2>
+          <button className={`${classes.getStartedBtn} ${classes.black}`}>Tag Your Project</button>
+        </section>
       </div>
-      <div className={classes.btnContainer}>
-        <button className={`${classes.btn} ${classes.black}`}>Tag Your Project</button>
-        <button className={classes.btn}>Learn More</button>
-      </div>
-    </section>
-    <section className={classes.ctiSection}>
-      <div className={classes.ctiContainer}>
-        <h2 className={classes.ctiHeading}>Civic Tech Index is used by:</h2>
-        <div className={classes.ctiImgsContainer}>
-          <a href='/'><img className={classes.ctiImgs} src='/images/code-for-all.png' alt='code for all' /></a>
-          <a href='/'><img className={classes.ctiImgs} src='/images/democracy-lab.svg' alt='democracy lab' /></a>
-          <a href='/'><img className={classes.ctiImgs} src='/images/code-for-america.svg' alt='code for america' /></a>
-          <a href='/'><img className={classes.ctiImgs} src='/images/yale-openlab.svg' alt='yale openlab' /></a>
-          <a href='/'><img className={classes.ctiImgs} src='/images/open-oakland.svg' alt='open oakland' /></a>
-          <a href='/'><img className={classes.ctiImgs} src='/images/hack-for-la.png' alt='hack for la' /></a>
-          <a href='/'><img className={classes.ctiImgs} src='/images/open-savannah.png' alt='open savannah' /></a>
-          <a href='/'><img className={classes.ctiImgs} src='/images/open-san-diego.svg' alt='open san diego' /></a>
-        </div>
-      </div>
-    </section>
-    <section className={classes.trendingTopicsSection}>
-      <div className={classes.trendingTopicsContainer}>
-        <h2 className={classes.trendingTopicsHeader}>How are people using the CTI?</h2>
-        <div className={classes.allTopicsContainer}>
-          <div className={classes.topicContainer}>
-            <a href='/'><p className={classes.trendingTopicsDetails}>Covid-19</p></a>
-          </div>
-          <div className={classes.topicContainer}>
-            <a href='/'><p className={classes.trendingTopicsDetails}>Trending Topic 1</p></a>
-          </div>
-          <div className={classes.topicContainer}>
-            <a href='/'><p className={classes.trendingTopicsDetails}>Trending Topic 2</p></a>
-          </div>
-          <div className={classes.topicContainer}>
-            <a href='/'><p className={classes.trendingTopicsDetails}>Trending Topic 3</p></a>
-          </div>
-          <div className={classes.topicContainer}>
-            <a href='/'><p className={classes.trendingTopicsDetails}>Trending Topic 4</p></a>
-          </div>
-          <div className={classes.topicContainer}>
-            <a href='/'><p className={classes.trendingTopicsDetails}>Trending Topic 5</p></a>
-          </div>
-        </div>
-      </div>
-    </section>
-    <section className={classes.getStartedSection}>
-      <h2 className={classes.getStartedHeading}>Ready to get started?</h2>
-      <button className={`${classes.getStartedBtn} ${classes.black}`}>Tag Your Project</button>
-    </section>
-    </div>
-    <Footer />
+      <Footer />
     </>
   )
 }
