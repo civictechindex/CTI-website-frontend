40967a2d8340901dcbf2a2ac9a60ee9c37148e11
nrrao <nandanarao@yahoo.com>
2020-11-09 07:21:57 -0500

    updated about page

--- src/pages/About/index.js
+++ src/pages/About/index.js
@@ -2,48 +2,85 @@ import Header from '../../components/Header'
 import Footer from '../../components/Footer'
 import React from 'react'
 import useStyles from './styles.js'
+import '../../styles.css'
 
 const About = () => {
   const classes = useStyles()
 
   return (
     <>
-    <Header />
-    <main className={classes.main}>
-      <section className={classes.headingSection}>
-        <div className={classes.headingContainer}>
-          <p className={classes.url}>Home / About</p>
-          <h1 className={classes.heading}>A movement to index every open source civic tech project on Github</h1>
-        </div>
-      </section>
-      <section className={classes.textSection}>
-        <h2 className={classes.text}>With your help, we can create a continuously updated repository for all civic tech enthusiasts to find open source projects to model, connect with, and learn from.</h2>
-      </section>
-      <section className={classes.projectsSection}>
-        <h3 className={classes.projectsHeading}>Easily search and clone projects from local and global brigades.</h3>
-        <div className={classes.projectsContainer}>
-          <div className={classes.projectsCard}>
-            <img className={classes.projectsImg1} src='/images/girl-coding.png' alt='girl coding on her computer' />
-            <p className={classes.projectsText1}>No coding experience needed to submit your project!</p>
+      <Header />
+      <div className={classes.page}>
+        <section className={classes.aboutSection}>
+          <div className={classes.urlDiv}>
+            <p className={classes.url}>Home / About</p>
+            </div>
+            <div className={classes.headingDiv}>
+            <h2 className={classes.heading}>
+              A movement to index every open source civic tech project on Github
+            </h2>
+    
           </div>
-          <div className={classes.projectsCard}>
-            <img className={classes.projectsImg2} src='/images/girl-and-guy-coding.png' alt='girl and guy coding' />
-            <p className={classes.projectsText2}>The index can be displayed and customized on any website</p>
+          <div className={classes.videoDiv}>
+            <img
+              className={classes.playButtonImg}
+              src="/images/playButton.png"
+              alt="Video play button"
+            />
           </div>
-          <div className={classes.projectsCard}>
-            <img className={classes.projectsImg3} src='/images/group-coding.png' alt='group coding' />
-            <p className={classes.projectsText3}>The index is owned by all who contribute to it</p>
+          <div className={classes.textSection}>
+            <h3 className={classes.text}>
+              With your help, we can create a continuously updated repository
+              for all civic tech enthusiasts to find open source projects to
+              model, connect with, and learn from.
+            </h3>
           </div>
-        </div>
-      </section>
-      <section className={classes.getStartedSection}>
-        <h2 className={classes.getStartedHeading}>Ready to get started?</h2>
-        <button className={`${classes.getStartedBtn} ${classes.black}`}>Tag Your Project</button>
-      </section>
-    </main>
-    <Footer />
+        </section>
+        <section className={classes.projectSection}>
+          <div className={classes.projectsContainer}>
+            <div className={classes.projectsCard}>
+              <img
+                className={classes.projectsImg1}
+                src="/images/girlCoding.png"
+                alt="girl coding on her computer"
+              />
+              <p className={classes.projectsText}>
+                No coding experience needed to submit your project!
+              </p>
+            </div>
+            <div className={classes.projectsCard}>
+              <img
+                className={classes.projectsImg2}
+                src="/images/girlandguyCoding.png"
+                alt="girl and guy coding"
+              />
+              <p className={classes.projectsText}>
+                Make your project more visible with GitHub’s open-source
+                communities
+              </p>
+            </div>
+            <div className={classes.projectsCard}>
+              <img
+                className={classes.projectsImg3}
+                src="/images/groupCodingColor.png"
+                alt="group coding"
+              />
+              <p className={classes.projectsText3}>
+                The Index is owned by all who contribute to it
+              </p>
+            </div>
+          </div>
+        </section>
+        <section className={classes.getStartedSection}>
+          <h2 className={classes.getStartedHeading}>Ready to get started?</h2>
+          <button className={`${classes.getStartedBtn} ${classes.black}`}>
+            Tag Your Project
+          </button>
+        </section>
+      </div>
+      <Footer />
     </>
-  )
+  );
 }
 
 export default About
--- src/pages/About/styles.js
+++ src/pages/About/styles.js
@@ -1,98 +1,160 @@
 import { createUseStyles } from 'react-jss'
 
 const useStyles = createUseStyles( theme => ({
-  main: {
+  page: {
     fontFamily: theme.primaryFontFamily,
-    color: theme.primaryTextColor,
-    '& section':{
       boxSizing: 'border-box',
-      paddingLeft: '20px',
-      paddingRight: '20px',
-    }
+      width: '100%',
   },
-/**********     HEADING SECTION      **********/
-  headingSection: {
+/**********     ABOUT SECTION      **********/
+  aboutSection: {
     width: '100%',
-    height: '350px',
-    backgroundColor: theme.backgroundColor1,
+    backgroundColor: '#004364',
+    //margin:'0px',
+      
   },
-  headingContainer: {
-    width: 'fit-content',
-    margin: '60px 240px 0 240px',
+  urlDiv:{
+    paddingTop:'24px',
+    paddingLeft: '16px',
+  },
+  url: {
+    fontSize: '12px',
+    lineHeight: '24px',
+    fontWeight: '400',
+    color:'#FEFEFE',
+    margin:'0px',
+  },
+  headingDiv:{
+    width:'300px',
+    margin:'auto',
+    paddingTop:'8px',
+    // paddingRight: '10px',
+    // paddingLeft: '10px',
+    // paddingBottom: '10px',
   },
   heading: {
-    fontSize: '36px',
-    fontWeight: 'bold',
-    lineHeight: '42px',
-    maxWidth: '570px',
-    marginTop: '60px'
+    fontSize: '21px',
+    fontWeight: '700',
+    lineHeight: '25px',
+    textAlign: 'center',
+  },
+  videoDiv:{
+    backgroundColor: '#F2F2F2',
+    width:'300px',
+    height:'128px',
+    marginTop:'10px',
+    margin:'auto',
+    paddingTop:'18px',
+    //paddingRight: '10px',
+    //paddingLeft: '10px',
+  },
+  playButtonImg: {
+    //transform: 'translate(0, 40px)',
+    marginLeft:'110px',
+    marginTop:'30px',
+    //margin:'auto',
   },
 /**********     TEXT SECTION      **********/
   textSection: {
     color: theme.secondaryTextColor,
-    backgroundColor: theme.backgroundColor4,
+    paddingTop:'64px',
+    paddingBottom:'41px',
     textAlign: 'center',
+    width: '300px',
+    margin:'auto',
   },
   text: {
     display: 'inline-block',
-    maxWidth: '770px',
-    fontSize: '26px',
-    lineHeight: '32px',
-    fontWeight: '400',
-    padding: '75px 0',
+    //width: '300px',
+    fontSize: '16px',
+    lineHeight: '20px',
+    fontWeight: '500',
+    //margin:'auto',
   },
 /**********     PROJCETS SECTION      **********/
-  projectsSection: {
-    backgroundColor: theme.backgroundColor2,
+  projectSection: {
+    backgroundColor: '#F2F2F2',
   },
-  projectsHeading: {
-    fontSize: '24px',
-    lineHeight: '32px',
-    maxWidth: '470px',
-    margin: '0',
-    padding: '60px 20px'
-  },
+  // projectsHeading: {
+  //   fontSize: '24px',
+  //   lineHeight: '32px',
+  //   //maxWidth: '470px',
+  //   margin: '0',
+  //   padding: '60px 20px'
+  // },
   projectsContainer: {
     display: 'flex',
-    justifyContent: 'space-around',
-    flexWrap: 'wrap',
+    flexDirection:'column',
+    // justifyContent: 'space-around',
+    // flexWrap: 'wrap',
     textAlign: 'center',
     paddingBottom: '40px',
   },
-  projectsCard: {
-    height: '293px',
-    margin: '15px'
+  // projectsCard: {
+  //   height: '293px',
+  //   margin: '15px'
+  // },
+  projectsImg1: {
+    marginTop:'25px',
+    width: '230px',
+    height: '218px',
+    //transform: 'translate(0, 40px)',
   },
   projectsImg2: {
-    transform: 'translate(0, 40px)',
+    width: '287px',
+    height: '167px',
+    marginTop:'47px',
+    //transform: 'translate(0, 40px)',
   },
   projectsImg3: {
-    transform: 'translate(0, 10px)',
+    width: '315px',
+    height: '212px',
+    marginTop:'20px',
+    //transform: 'translate(0, 10px)',
   },
-  projectsText1: {
-    width: '270px',
+  projectsText: {
+    width: '250px',
+    textAlign: 'center',
+    margin:'auto',
+    fontSize: '14px',
+    lineHeight: '18px',
+    fontWeight: '500',
   },
-  projectsText2: {
-    width: '303px',
-    transform: 'translate(0, 42px)',
-  },
+  // projectsText2: {
+  //   width: '303px',
+  //   transform: 'translate(0, 42px)',
+  // },
   projectsText3: {
-    width: '270px',
-    transform: 'translate(13%, 11px)',
+    width: '200px',
+    textAlign: 'center',
+    margin:'auto',
+    marginBottom:'10px',
+    fontSize: '14px',
+    lineHeight: '18px',
+    fontWeight: '500',
+    //transform: 'translate(13%, 11px)',
   },
 /**********     GET-STARTED SECTION     **********/
   getStartedSection: {
     backgroundColor: theme.backgroundColor1,
-    paddingTop: '60px',
-    paddingBottom: '60px',
-    margin: '0 10px',
-    textAlign: 'center',
+    // paddingTop: '31px',
+    // paddingBottom:'40px',
+    display: 'flex',
+    flexDirection:'column',
+    justifyContent: 'space-around',
+    margin: 'auto',
+    height: '176px',
   },
   getStartedHeading: {
     fontWeight: '500',
-    fontSize: '36px',
-    margin: '0', 
-    marginBottom: '30px', 
+    fontSize: '20px',
+    lineHeight: '24px',
+    color:'#042D5F',
+    textAlign: 'center',
+    marginRight: 'auto',
+    marginLeft: 'auto',
+    marginTop: '32px', 
+    marginBottom: '16px', 
   },
   getStartedBtn: {
     display: 'inline-block',
@@ -102,47 +164,218 @@ const useStyles = createUseStyles( theme => ({
     border: `1px solid ${theme.button1BG}`,
     backgroundColor: theme.button1BG,
     borderRadius: '22.5px',
-    width: '270px',
+    width: '200px',
     height: '45px',
     outline: 'none',
     cursor: 'pointer',
+    margin: 'auto',
+    marginBottom: '40px', 
   },
 /**********     MEDIA-QUERIES     **********/
-  '@media (max-width: 672px)': {
-    projectsContainer: {
-      paddingBottom: '100px'
-    },
-    projectsImg3: {
-      marginTop: '45px',
-    },
+'@media (min-width: 675px)': {
+  url: {
+    fontSize: '12px',
   },
-  '@media (max-width: 935px)': {
-    headingContainer: {
-      margin: '50px auto 0 auto'
-    },
-    heading: {
-      maxWidth: '426px',
-    }
+  headingDiv:{
+    width:'432px',
   },
-  '@media (max-width: 450px)': {
-    heading: {
-      fontSize: '26px',
-      lineHeight: '36px',
-    },
-    text: {
-      fontSize: '20px',
-      lineHeight: '28px',
-    },
-    projectsHeading: {
-      fontSize: '20px',
-      lineHeight: '28px',
-    },
+  heading: {
+    fontSize: '28px',
+    lineHeight: '32px',
   },
-  '@media (max-width: 460px)': {
-    getStartedHeading: {
-      fontSize: '30px',
-    },
+  videoDiv:{
+    backgroundColor: '#F2F2F2',
+    width:'500px',
+    height:'275px',
+    marginTop:'55px',
+    margin:'auto',
+    //paddingTop:'18px',
+    //paddingRight: '10px',
+    //paddingLeft: '10px',
   },
+  playButtonImg: {
+    height:'85px',
+    width:'85px',
+    //transform: 'translate(0, 40px)',
+    marginLeft:'206px',
+    marginTop:'95px',
+    //margin:'auto',
+  },
+  /**********     TEXT SECTION      **********/
+  textSection: {
+    paddingTop:'56px',
+    paddingBottom:'83px',
+    width: '500px',
+    margin:'auto',
+  },
+  projectsImg1: {
+    marginTop:'40px',
+    width: '273px',
+    height: '259px',
+    //transform: 'translate(0, 40px)',
+  },
+  projectsImg2: {
+    width: '327px',
+    height: '191px',
+    marginTop:'58px',
+    //transform: 'translate(0, 40px)',
+  },
+  projectsImg3: {
+    width: '380px',
+    height: '256px',
+    marginTop:'36px',
+    //transform: 'translate(0, 10px)',
+  },
+  projectsText: {
+    width: '340px',
+    textAlign: 'center',
+    margin:'auto',
+    fontSize: '18px',
+    lineHeight: '24px',
+    fontWeight: '500',
+  },
+  // projectsText2: {
+  //   width: '303px',
+  //   transform: 'translate(0, 42px)',
+  // },
+  projectsText3: {
+    width: '270px',
+    textAlign: 'center',
+    margin:'auto',
+    marginBottom:'10px',
+    fontSize: '18px',
+    lineHeight: '24px',
+    fontWeight: '500',
+    //transform: 'translate(13%, 11px)',
+  },
+  // getStartedSection: {
+  //   height: '240px',
+  // },
+  getStartedHeading: {
+    fontWeight: '500',
+    fontSize: '36px',
+    lineHeight: '42px',
+    color:'#042D5F',
+    textAlign: 'center',
+    marginRight: 'auto',
+    marginLeft: 'auto',
+    marginTop: '56px', 
+    marginBottom: '24px', 
+  },
+  getStartedBtn: {
+    display: 'inline-block',
+    width: '250px',
+    height: '48px',
+    marginBottom: '70px', 
+  },
+},
+
+'@media (min-width: 900px)': {
+  urlDiv:{
+    paddingTop:'40px',
+    paddingLeft: '152px',
+  },
+  url: {
+    fontSize: '14px',
+    
+  },
+  headingDiv:{
+    width:'944px',
+  },
+  heading: {
+    fontSize: '36px',
+    lineHeight: '38px',
+    //paddingRight: '248px',
+    //paddingLeft: '248px',
+  },
+  videoDiv:{
+    //backgroundColor: '#F2F2F2',
+    width:'944px',
+    height:'440px',
+    marginTop:'32px',
+    margin:'auto',
+    //paddingTop:'18px',
+    //paddingRight: '10px',
+    //paddingLeft: '10px',
+  },
+ 
+  playButtonImg: {
+    height:'121px',
+    width:'121px',
+    //transform: 'translate(0, 40px)',
+    marginLeft:'412px',
+    marginTop:'160px',
+    //margin:'auto',
+  },
+ 
+  textSection: {
+    width:'750px',
+  },
+  text: {
+    display: 'inline-block',
+    //width: '300px',
+    fontSize: '24px',
+    lineHeight: '32px',
+    fontWeight: '500',
+    //margin:'auto',
+  },
+    projectSection: {
+    backgroundColor: '#004364',
+    paddingBottom:'56px',
+    
+  },
+  projectsContainer: {
+    margin:'auto',
+    width: '1182px',
+    display: 'flex',
+    flexDirection:'row',
+    justifyContent: 'space-around',
+    //flexWrap: 'wrap',
+    textAlign: 'center',
+    //paddingBottom: '40px',
+    backgroundColor: '#FEFEFE',
+  },
+  projectsImg1: {
+    marginTop:'25px',
+    width: '230px',
+    height: '218px',
+    //transform: 'translate(0, 40px)',
+  },
+  projectsImg2: {
+    width: '287px',
+    height: '167px',
+    marginTop:'47px',
+    //transform: 'translate(0, 40px)',
+  },
+  projectsImg3: {
+    width: '315px',
+    height: '212px',
+    marginTop:'20px',
+    //transform: 'translate(0, 10px)',
+  },
+  projectsText: {
+    width: '250px',
+    textAlign: 'center',
+    margin:'auto',
+    fontSize: '14px',
+    lineHeight: '18px',
+    fontWeight: '500',
+  },
+  // projectsText2: {
+  //   width: '303px',
+  //   transform: 'translate(0, 42px)',
+  // },
+  projectsText3: {
+    width: '200px',
+    textAlign: 'center',
+    margin:'auto',
+    marginBottom:'10px',
+    fontSize: '14px',
+    lineHeight: '18px',
+    fontWeight: '500',
+    //transform: 'translate(13%, 11px)',
+  },
+}
 }))
 
 export default useStyles
