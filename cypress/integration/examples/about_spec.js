83099a5ab28c7ab5f26171a69b5ad4ef712a9c9c
nrrao <nandanarao@yahoo.com>
2020-12-14 19:26:11 -0500

    updated video

--- cypress/integration/examples/about_spec.js
+++ cypress/integration/examples/about_spec.js
@@ -7,26 +7,26 @@ context('Actions', () => {
 
   it('verify header loading', () => {
     cy.contains("Join the Index")
-})
+  })
 
-it('verify footer loading', () => {
+  it('verify footer loading', () => {
     cy.contains('The Civic Tech Index is an open-source project.')
-})
+  })
 
   it('verify title section loads', () => {
     cy.contains('A movement to index');
   })
 
-  
-  it ('verify marketing section loads', () => {
+
+  it('verify marketing section loads', () => {
     cy.contains('With your help')
   })
 
-    it ('verify picture card loads', () => {
-      cy.contains('No coding experience needed to submit your project!')
-    })
+  it('verify picture card loads', () => {
+    cy.contains('No coding experience needed to submit your project!')
+  })
 
-      it ('verify get started card loads', () => {
-        cy.contains('Ready to get started?')
-})
+  it('verify get started card loads', () => {
+    cy.contains('Ready to get started?')
+  })
 })
