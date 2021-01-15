d4402736c7c4eb1df64bfacda99efad37f6b6906
Kevin Ashworth <kevinashworth@yahoo.com>
2021-01-15 08:17:36 -0800

    Fix lint errors and warnings in `cypress` folder

--- cypress/integration/pages/home.spec.js
+++ cypress/integration/pages/home.spec.js
@@ -1,34 +1,33 @@
 context('Actions', () => {
-    beforeEach(() => {
-      cy.visit('/home')
-    })
+  beforeEach(() => {
+    cy.visit('/home')
+  })
 
-    it('verify header loading', () => {
-        cy.contains("Join the Index")
-    })
+  it('verify header loading', () => {
+    cy.contains("Join the Index")
+  })
 
-    it('verify footer loading', () => {
-        cy.contains('The Civic Tech Index is an open-source project.')
-    })
-  
-    it('verify title section loads', () => {
-        cy.contains('Join a worldwide movement to catalog every open source civic tech project.')
-    })
+  it('verify footer loading', () => {
+    cy.contains('The Civic Tech Index is an open-source project.')
+  })
 
-    it ('verify marketing section loads', () => {
-        cy.contains('Requires no coding')
-    })
+  it('verify title section loads', () => {
+    cy.contains('Join a worldwide movement to catalog every open source civic tech project.')
+  })
 
-    it ('verify notable users section loads', () => {
-        cy.contains('See how the Civic Tech Index is used by')
-    })
+  it ('verify marketing section loads', () => {
+    cy.contains('Requires no coding')
+  })
 
-    it ('verify trending topics loads', () => {
-        cy.contains('How are people using the CTI?')
-    })
+  it ('verify notable users section loads', () => {
+    cy.contains('See how the Civic Tech Index is used by')
+  })
 
-    it('verify get started card loads', () => {
-        cy.contains('Ready to get started?')
-      })
+  it ('verify trending topics loads', () => {
+    cy.contains('How are people using the CTI?')
+  })
+
+  it('verify get started card loads', () => {
+    cy.contains('Ready to get started?')
+  })
 })
-  
