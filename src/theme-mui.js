e5db48c90213358ab2379e53fcdeef0e0ba9340e
Kevin Ashworth <kevinashworth@yahoo.com>
2021-01-20 13:21:04 -0800

    Add MUI theme to src/index
    - Use MUI baseline (called reset in some packages)
    - Add body, fontFamily, h1, h2, and h5 as in styles.css and src/index.js
    - For the time being, keep other themes

---
+++ src/theme-mui.js
@@ -0,0 +1,49 @@
+/* eslint-disable sort-keys */
+
+const theme = {
+  overrides: {
+    MuiCssBaseline: {
+      '@global': {
+        body: {
+          fontStyle: 'normal',
+          margin: 0,
+        },
+      },
+    },
+  },
+  typography: {
+    fontFamily: 'Work Sans, sans-serif',
+    h1: {
+      color: '#FFE06D',
+      fontSize: '42px',
+      fontWeight: '700',
+      letterSpacing: '0px',
+      lineHeight: '48px',
+      marginBottom: '0.825em',
+      marginTop: '0.825em',
+    },
+    h2: {
+      color: '#FFE06D',
+      fontFamily: 'Work Sans',
+      fontStyle: 'normal',
+      fontWeight: 'bold',
+      fontSize: '28px',
+      lineHeight: '38px',
+      marginBottom: '0.825em',
+      marginTop: '0.825em',
+    },
+    h5: {
+      color: '#FFFFFF',
+      fontFamily: 'Work Sans',
+      fontSize: '24px',
+      fontStyle: 'normal',
+      fontWeight: '700',
+      lineHeight: '30px',
+      marginBottom: '1.675em',
+      marginTop: '1.675em',
+      textAlign: 'left',
+    },
+  },
+}
+
+export default theme
