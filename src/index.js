bfe4ea60ef823469a47ea8610e8fc049690e4029
Emily M <30880308+e0marina@users.noreply.github.com>
2021-02-01 13:18:26 -0800

    Fixed issue #262

--- src/index.js
+++ src/index.js
@@ -2,11 +2,9 @@ import React from 'react'
 import ReactDOM from 'react-dom'
 import App from './App'
 import * as serviceWorker from './serviceWorker'
-import { ThemeProvider } from 'react-jss'
 import jss from 'jss'
 import preset from 'jss-preset-default'
-import { SheetsRegistry } from 'react-jss'
-import { JssProvider } from 'react-jss'
+import { SheetsRegistry, JssProvider, ThemeProvider  } from 'react-jss'
 import { BrowserRouter } from 'react-router-dom'
 import WebFont from 'webfontloader'
 
@@ -43,25 +41,25 @@ const setupJss = () => {
 const sheets = setupJss()
 
 const theme = {
-  primaryFontFamily: 'Work Sans, san-serif',
-  secondaryFontFamily: 'Space Mono, monospace',
-  thirdFontFamily: 'Source Code Pro, monospace',
-  primaryTextColor: 'black',
-  secondaryTextColor: 'white',
-  footerBG: '#242424',
-  headerBG: '#D8D8D8',
-  headerBG2: '#F8F8F8',
-  headerCircleBG: '#F4F4F4',
-  button1BG: '#0CB2E7',
-  button1TextColor: 'white',
-  button2BG: '#FFFFFF',
-  button2TextColor: '#979797',
   backgroundColor1: '#FFFFFF',
   backgroundColor2: '#F2F2F2',
   backgroundColor3: '#E1E1E1',
   backgroundColor4: '#242424',
   backgroundColor5: '#E9E9E9',
   backgroundColor6: '#C4C4C4',
+  button1BG: '#0CB2E7',
+  button1TextColor: 'white',
+  button2BG: '#FFFFFF',
+  button2TextColor: '#979797',
+  footerBG: '#242424',
+  headerBG: '#D8D8D8',
+  headerBG2: '#F8F8F8',
+  headerCircleBG: '#F4F4F4',
+  primaryFontFamily: 'Work Sans, san-serif',
+  primaryTextColor: 'black',
+  secondaryFontFamily: 'Space Mono, monospace',
+  secondaryTextColor: 'white',
+  thirdFontFamily: 'Source Code Pro, monospace',
 }
 
 ReactDOM.render(
