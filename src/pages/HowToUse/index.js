b895bb176326b7e25214ca52df4b87a9d36ba2fe
Emily M <30880308+e0marina@users.noreply.github.com>
2021-02-02 14:41:01 -0800

    redoing PR for issue 264

--- src/pages/HowToUse/index.js
+++ src/pages/HowToUse/index.js
@@ -1,14 +1,12 @@
-import React from 'react'
-import { Link } from 'react-router-dom'
-
-import BottomCallToAction from '../../components/BottomCallToAction'
-import Header from '../../components/Header'
-import Footer from '../../components/Footer'
-
-import useStyles from './styles.js'
+import React from 'react';
+import { Link } from 'react-router-dom';
+import BottomCallToAction from '../../components/BottomCallToAction';
+import Header from '../../components/Header';
+import Footer from '../../components/Footer';
+import useStyles from './styles.js';
 
 const HowToUse = () => {
-  const classes = useStyles()
+  const classes = useStyles();
 
   return (
     <>
@@ -16,34 +14,87 @@ const HowToUse = () => {
       <main className={classes.main}>
         <section className={classes.projectsSection}>
           <div className={classes.projectsContainer}>
-            <p className={classes.projectsLink}>About / Adding Projects to the Index</p>
-            <h1 className={classes.projectsHeading}>Adding Projects to the Index</h1>
-            <h6 className={classes.projectsSubHeading}>Generating Topics/Tags for your Civic Tech Project</h6>
-            <p className={classes.projectsSubHeadingDetails}>We recommend having your project&apos;s repository open in another browser for ease of convenience</p>
+            <p className={classes.projectsLink}>
+              About / Adding Projects to the Index
+            </p>
+            <h1 className={classes.projectsHeading}>
+              Adding Projects to the Index
+            </h1>
+            <h6 className={classes.projectsSubHeading}>
+              Generating Topics/Tags for your Civic Tech Project
+            </h6>
+            <p className={classes.projectsSubHeadingDetails}>
+              We recommend having your project&apos;s repository open in another
+              browser for ease of convenience
+            </p>
             <ol className={classes.projectsList}>
-              <li>On the Civic Tech Index <Link to='/' className={classes.link}>Tag Generator,</Link> navigate to the form and complete the following fields</li>
+              <li>
+                On the Civic Tech Index{' '}
+                <Link to='/' className={classes.link}>
+                  Tag Generator,
+                </Link>{' '}
+                navigate to the form and complete the following fields
+              </li>
               <textarea></textarea>
-              <li>To create tags, type in keywords that describe your topic(s), causes(s), or civic issue(s) that your project addresses.</li>
+              <li>
+                To create tags, type in keywords that describe your topic(s),
+                causes(s), or civic issue(s) that your project addresses.
+              </li>
               <textarea></textarea>
               <li>Copy the tages that appear below each keyword.</li>
               <textarea></textarea>
-              <li>Navigate to your project&apos;s repository in another browser to add your generated tags.</li>
-              <li>Under your project&apos;s repository, click <span className={classes.bold}>Manage Topics</span> to paste your tags.</li>
+              <li>
+                Navigate to your project&apos;s repository in another browser to
+                add your generated tags.
+              </li>
+              <li>
+                Under your project&apos;s repository, click{' '}
+                <span className={classes.bold}>Manage Topics</span> to paste
+                your tags.
+              </li>
               <div className={classes.imgContainer}>
-                <img className={classes.img} src='/images/how-to-use-step-1.png' alt='first step on how to manage your tags in github' />
-                <p className={`${classes.imgText} ${classes.gray}`}>civictechindex <span className={classes.black}>/</span> <strong>civictechindex-launch</strong></p>
+                <img
+                  className={classes.img}
+                  src='/images/how-to-use-step-1.png'
+                  alt='first step on how to manage your tags in github'
+                />
+                <p className={`${classes.imgText} ${classes.gray}`}>
+                  civictechindex <span className={classes.black}>/</span>{' '}
+                  <strong>civictechindex-launch</strong>
+                </p>
               </div>
               <div className={classes.imgContainer}>
-                <img className={classes.img} src='/images/how-to-use-step-2.png' alt='second step on how to manage your tags in gitbub' />
-                <p className={`${classes.imgText} ${classes.gray}`}>civictechindex <span className={classes.black}>/</span> <strong>civictechindex-launch</strong></p>
+                <img
+                  className={classes.img}
+                  src='/images/how-to-use-step-2.png'
+                  alt='second step on how to manage your tags in gitbub'
+                />
+                <p className={`${classes.imgText} ${classes.gray}`}>
+                  civictechindex <span className={classes.black}>/</span>{' '}
+                  <strong>civictechindex-launch</strong>
+                </p>
               </div>
-              <li>Repeat until you have finished adding all of your tags, then click <span className={classes.bold}>Done</span> to save your new topics.</li>
+              <li>
+                Repeat until you have finished adding all of your tags, then
+                click <span className={classes.bold}>Done</span> to save your
+                new topics.
+              </li>
               <div className={classes.imgContainer}>
-                <img className={classes.img} src='/images/how-to-use-step-3.png' alt='last step on how to manage your tags in gitbub' />
-                <p className={classes.imgText}>civictechindex <span className={classes.black}>/</span> <strong>civictechindex-launch</strong></p>
+                <img
+                  className={classes.img}
+                  src='/images/how-to-use-step-3.png'
+                  alt='last step on how to manage your tags in gitbub'
+                />
+                <p className={classes.imgText}>
+                  civictechindex <span className={classes.black}>/</span>{' '}
+                  <strong>civictechindex-launch</strong>
+                </p>
               </div>
             </ol>
-            <p className={classes.projectsFooter}>Don&apos;t forget to tell us when you&apos;ve submitted a project or if you would like your organization added to our network!</p>
+            <p className={classes.projectsFooter}>
+              Don&apos;t forget to tell us when you&apos;ve submitted a project
+              or if you would like your organization added to our network!
+            </p>
           </div>
         </section>
         <BottomCallToAction
@@ -54,7 +105,7 @@ const HowToUse = () => {
       </main>
       <Footer />
     </>
-  )
-}
+  );
+};
 
-export default HowToUse
+export default HowToUse;
