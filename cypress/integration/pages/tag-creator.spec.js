e95e6c38ce0b4d8fdc61de632b1324e30017087c
Kevin Ashworth <kevinashworth@yahoo.com>
2021-02-07 13:48:41 -0800

    Add to TagCreator spec a test for affiliated

--- cypress/integration/pages/tag-creator.spec.js
+++ cypress/integration/pages/tag-creator.spec.js
@@ -1,6 +1,15 @@
 describe('Tag Creator (Tag Generator)', () => {
-  const TEST_URL = 'https://github.com/civictechindex/CTI-website-frontend.git'
-  const TEST_TAGS = [
+  const AFFILIATED_ORGANIZATION = 'Code for Boston'
+  const AFFILIATED_TEST_URL = 'codeforboston / voiceapp311'
+  const AFFILIATED_TEST_TAGS = [
+    'code-for-boston',
+    'code-for-america',
+    'alexa',
+    'trash',
+  ]
+
+  const UNAFFILIATED_TEST_URL = 'https://github.com/civictechindex/CTI-website-frontend.git'
+  const UNAFFILIATED_TEST_TAGS = [
     'civictechindex',
     'hack-for-la',
     'code-for-america',
@@ -10,12 +19,8 @@ describe('Tag Creator (Tag Generator)', () => {
     'opensource',
   ]
 
-  before(() => {
-    cy.visit('/tag-generator')
-  })
-
   beforeEach(() => {
-    // cy.visit('/tag-generator')
+    cy.visit('/tag-generator')
   })
 
   it('loads', () => {
@@ -23,15 +28,30 @@ describe('Tag Creator (Tag Generator)', () => {
       .contains('Tag Generator')
   })
 
-  it('loads correct tags for `civictechindex/CTI-website-frontend` repo - unaffiliated', () => {
-    cy.get('[data-cy=radio-unaffiliated]').click()
-    cy.get('#container-unaffiliated').within(() => {
-      cy.get('#repository-url').click().type(TEST_URL).type('{enter}')
+  it('loads correct 4 tags for `codeforboston/voiceapp311` - affiliated', () => {
+    cy.get('[data-cy=radio-affiliated]').click()
+    cy.get('#container-affiliated').within(() => {
+      cy.get('#organization').click().type(AFFILIATED_ORGANIZATION).type('{downarrow}{enter}')
+      cy.get('#repository-url', { force: true }).click().type(AFFILIATED_TEST_URL).type('{enter}')
       cy.get('[data-cy=current-tags]').within(() => {
         cy.get('[data-cy=topic-tag] span').each(($el, index, $list) => {
           const innerText = $el.text()
-          expect(TEST_TAGS.indexOf(innerText)).to.be.eq(index)
-          expect(TEST_TAGS.indexOf(innerText)).to.be.gt(-1)
+          expect(AFFILIATED_TEST_TAGS.indexOf(innerText)).to.be.eq(index)
+          // expect(AFFILIATED_TEST_TAGS.indexOf(innerText)).to.be.gt(-1)
+        })
+      })
+    })
+  })
+
+  it('loads correct 7 tags for `civictechindex/CTI-website-frontend` - unaffiliated', () => {
+    cy.get('[data-cy=radio-unaffiliated]').click()
+    cy.get('#container-unaffiliated').within(() => {
+      cy.get('#repository-url').click().type(UNAFFILIATED_TEST_URL).type('{enter}')
+      cy.get('[data-cy=current-tags]').within(() => {
+        cy.get('[data-cy=topic-tag] span').each(($el, index, $list) => {
+          const innerText = $el.text()
+          expect(UNAFFILIATED_TEST_TAGS.indexOf(innerText)).to.be.eq(index)
+          // expect(UNAFFILIATED_TEST_TAGS.indexOf(innerText)).to.be.gt(-1)
         })
       })
     })
