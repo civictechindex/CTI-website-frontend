ca23775f27ad67421b4423dabf6dd34e9b9fe472
Kevin Ashworth <kevinashworth@yahoo.com>
2021-02-10 08:07:03 -0800

    Add spec for Donate and Header; improve FAQ spec
    These were lowest sections on the overall report:
    
    BEFORE
    84.98% Statements 413/486
    71.74% Branches 99/138
    81.33% Functions 122/150
    85.04% Lines 398/468
    
    AFTER
    90.53% Statements 440/486
    76.09% Branches 105/138
    90% Functions 135/150
    90.38% Lines 423/468

--- cypress/integration/pages/faq.spec.js
+++ cypress/integration/pages/faq.spec.js
@@ -1,9 +1,25 @@
 describe('FAQ Page', () => {
+  const SEARCH = 'ask'
+  const Q = 'What should I ask?'
+  const A = 'Anything you like'
+
   before(() => {
     cy.visit('/radicalcollaboration/faq')
   })
 
   it('title section loads', () => {
     cy.contains('How can we help?');
+  })
+
+  it('gets faq by search', () => {
+    cy.get('[data-cy=search-faq]').click().type(SEARCH)
+    cy.get('[data-cy=faq-question]')
+      .first()
+      .contains(Q)
+    cy.get('[data-cy=faq-question]')
+      .first()
+      .click()
+    cy.get('[data-cy=faq-answer]')
+      .contains(A)
   })
 })
