0fb52fa08f889bb298e703e87d9025040bf00d99
Christian Ipanaque <christian.ipanaque@formidable.com>
2021-02-14 23:29:52 -0800

    replace the value of 0 to false, since 0 is not an accepted value to this prop according to Grids xs PropType

--- src/components/Footer/index.js
+++ src/components/Footer/index.js
@@ -42,7 +42,7 @@ const Footer = () => {
   return (
     <div>
       <Grid container className={classes.footerContainer}>
-        <Grid item xs={0} sm={1} />
+        <Grid item xs={false} sm={1} />
         <Grid item xs={4} sm={2}>
           <Link to='/'>Join the Index</Link>
           <Link to='/tag-generator'>Tag Generator</Link>
@@ -66,7 +66,7 @@ const Footer = () => {
         <Grid item xs={6} sm={2}>
           <SocialSection />
         </Grid>
-        <Grid item xs={0} sm={1} />
+        <Grid item xs={false} sm={1} />
         <Grid item xs={12} className={classes.noteContainer}>
           <p>The Civic Tech Index is an open-source project.</p>
           <p>You can download or contribute to the code on <a href='https://github.com/civictechindex'>GitHub</a>.</p>
