4b2accb7d2a04c414dc51e9a20fa1a4fd7d5f1dc
Kevin Ashworth <kevinashworth@yahoo.com>
2021-02-18 07:12:39 -0800

    In About, use Box and Container with MUI attributes, rather than in-line styling
    NOTE: We currently don't have a Typography size that matches Figma for "With your help ...". We either should take a look at the use of all 6 header tags, or body2, or something.

--- src/pages/About.js
+++ src/pages/About.js
@@ -1,12 +1,11 @@
 import React from "react";
-import { Card, CardMedia, Container, Grid } from "@material-ui/core";
+import { Box, Card, CardMedia, Container, Typography } from "@material-ui/core";
 import {
   GetStartedCard,
   NavBreadcrumbs,
   PictureCard,
   TitleSection,
 } from "../components";
-import "../styles.css";
 
 const About = () => {
   const crumbs = [
@@ -22,30 +21,30 @@ const About = () => {
 
   const VideoSection = () => {
     return (
-      <Grid container alignItems="center" justify="center" style={{ margin: 'auto', width: '70%', backgroundColor: 'white' }}>
+      <Container maxWidth='lg'>
         <Card>
           <CardMedia component="video" image='/images/CTI V1.mp4' title='Overview of CTI' controls />
         </Card>
-      </Grid>
+      </Container>
     )
   }
 
   const MarketingSection = () => {
     return (
-      <Grid container justify="center" alignItems="center" >
-        <Grid item xs={12} md={8}>
-          <p className='white' style={{ padding: '55px 30px 55px 30px', margin: 'auto' }}>
+      <Container maxWidth='md'>
+        <Box py={6}>
+          <Typography variant='h6' color='textSecondary' align='center'>
             With your help, we can create a continuously updated repository for all civic
             tech enthusiasts to find open source projects to model, connect with, and learn from.
-          </p>
-        </Grid>
-      </Grid>
+          </Typography>
+        </Box>
+      </Container>
     )
   }
 
   return (
     <Container className="containerDefault">
-      <NavBreadcrumbs crumbs={crumbs} color="#FEFEFE" />
+      <NavBreadcrumbs crumbs={crumbs} />
       <TitleSection>
         A movement to index every open source civic tech project on GitHub
       </TitleSection>
