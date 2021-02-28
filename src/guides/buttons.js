03759cdff52930999e7ab531f02fdd7811242b7d
Kevin Ashworth <kevinashworth@yahoo.com>
2021-02-28 13:37:16 -0800

    Guides: unique URL per guide (#366)
    * Make Guides suitable for Layout
    
    * Add `:guide` param to distinguish each guide by the URL
    
    Now you can link to the guides, with URLs:
    /guides/colors
    /guides/type-standards
    /guides/typography
    
    * Better layout on mobile
    
    * Revert src/index.js changes (changes are from another PR, not yet merged)
    
    * Change global Button settings per Figma design and new Guide for Buttons
    
    Button should be used with color='primary' or color='secondary'
    
    * Better format of Guides and run Prettier
    
    Co-authored-by: Olivia Chiong <75456721+Olivia-Chiong@users.noreply.github.com>
    Co-authored-by: Brent Hengeveld <jafar.ironclad@gmail.com>

---
+++ src/guides/buttons.js
@@ -0,0 +1,98 @@
+import React, { useState } from 'react';
+import Button from '@material-ui/core/Button';
+import Card from '@material-ui/core/Card';
+import CardContent from '@material-ui/core/CardContent';
+import Dialog from '@material-ui/core/Dialog';
+import DialogActions from '@material-ui/core/DialogActions';
+import DialogContent from '@material-ui/core/DialogContent';
+import DialogContentText from '@material-ui/core/DialogContentText';
+import Grid from '@material-ui/core/Grid';
+import Typography from '@material-ui/core/Typography';
+import { makeStyles } from '@material-ui/core/styles';
+
+const useStyles = makeStyles((theme) => ({
+  textGridContainer: {
+    paddingBottom: theme.spacing(4),
+  },
+}));
+
+const ShowButtons = () => {
+  const [open, setOpen] = useState(false);
+  const handleOpen = () => {
+    setOpen(true);
+  };
+  const handleClose = () => {
+    setOpen(false);
+  };
+  const classes = useStyles();
+
+  return (
+    <>
+      <Dialog open={open} onClose={handleClose}>
+        <DialogContent>
+          <DialogContentText color='primary'>The button press caused this dialog to open.</DialogContentText>
+        </DialogContent>
+        <DialogActions>
+          <Button color='secondary' onClick={handleClose}>
+            Close
+          </Button>
+        </DialogActions>
+      </Dialog>
+      <Typography variant='h1'>Buttons</Typography>
+      <Grid container spacing={2} className={classes.textGridContainer}>
+        <Grid item xs={12} sm={6} lg={3}>
+          <Card className='card240'>
+            <CardContent>
+              <Button onClick={handleOpen}>Default</Button>
+            </CardContent>
+          </Card>
+        </Grid>
+        <Grid item xs={12} sm={6} lg={3}>
+          <Card className='card244'>
+            <CardContent>
+              <Button disabled>Default Disabled</Button>
+            </CardContent>
+          </Card>
+        </Grid>
+        <Grid item xs={12} sm={6} lg={3}>
+          <Card className='card241'>
+            <CardContent>
+              <Button color='secondary' onClick={handleOpen}>
+                Secondary
+              </Button>
+            </CardContent>
+          </Card>
+        </Grid>
+        <Grid item xs={12} sm={6} lg={3}>
+          <Card className='card242'>
+            <CardContent>
+              <Button color='secondary' disabled>
+                Secondary Disabled
+              </Button>
+            </CardContent>
+          </Card>
+        </Grid>
+        <Grid item xs={12} sm={6} lg={3}>
+          <Card className='card240'>
+            <CardContent>
+              <Button color='primary' onClick={handleOpen}>
+                Primary (Default)
+              </Button>
+            </CardContent>
+          </Card>
+        </Grid>
+        <Grid item xs={12} sm={6} lg={3}>
+          <Card className='card244'>
+            <CardContent>
+              <Button color='primary' disabled>
+                Primary Disabled
+              </Button>
+            </CardContent>
+          </Card>
+        </Grid>
+      </Grid>
+    </>
+  );
+};
+
+export default ShowButtons;
--- src/guides/index.js
+++ src/guides/index.js
@@ -1,75 +1,72 @@
-import React from 'react'
-import Container from '@material-ui/core/Container'
-import FormControl from '@material-ui/core/FormControl'
-import Grid from '@material-ui/core/Grid'
-import InputLabel from '@material-ui/core/InputLabel'
-import MenuItem from '@material-ui/core/MenuItem'
-import Select from '@material-ui/core/Select'
-import ExpandMoreRoundedIcon from '@material-ui/icons/ExpandMoreRounded'
-import { makeStyles } from '@material-ui/core/styles'
-import { Footer, Header } from '../components'
-import Colors from './colors'
-import TypeStandards from './type-standards'
-import Typography from './typography'
+import React from 'react';
+import { withRouter } from 'react-router-dom';
+import Container from '@material-ui/core/Container';
+import FormControl from '@material-ui/core/FormControl';
+import Grid from '@material-ui/core/Grid';
+import InputLabel from '@material-ui/core/InputLabel';
+import MenuItem from '@material-ui/core/MenuItem';
+import Select from '@material-ui/core/Select';
+import ExpandMoreRoundedIcon from '@material-ui/icons/ExpandMoreRounded';
+import { makeStyles } from '@material-ui/core/styles';
+import Buttons from './buttons';
+import Colors from './colors';
+import TypeStandards from './type-standards';
+import Typography from './typography';
 
 const useStyles = makeStyles((theme) => ({
   container: {
     backgroundColor: theme.palette.background.primary,
-    padding: theme.spacing(4)
+    padding: theme.spacing(3),
   },
   gridItem0: {
-    backgroundColor: theme.palette.background.secondary
+    backgroundColor: theme.palette.background.secondary,
   },
   gridItem1: {
-    backgroundColor: theme.palette.background.default
+    backgroundColor: theme.palette.background.default,
   },
-}))
+}));
 
-const Guides = () => {
-  const classes = useStyles()
-  const [guide, setGuide] = React.useState('colors')
+const Guides = (props) => {
+  const { history, match } = props;
+  const classes = useStyles();
+  const [guide, setGuide] = React.useState(match.params.guide || 'colors');
 
   const handleChange = (event) => {
-    event.target.value && setGuide(event.target.value)
-  }
+    setGuide(event.target.value);
+    history.push('/guides/' + event.target.value);
+  };
 
   const components = {
+    buttons: Buttons,
     colors: Colors,
-    palette: TypeStandards,
+    'type-standards': TypeStandards,
     typography: Typography,
-  }
-  const Guide = components[guide]
+  };
+  const Guide = components[guide];
 
   return (
-    <>
-      <Header />
-      <Container className={classes.container} maxWidth={false}>
-        <Grid container spacing={2}>
-          <Grid item xs={2} className={classes.gridItem0}>
-            <FormControl>
-              <InputLabel id='select-a-guide'>Guides</InputLabel>
-              <Select
-                autoWidth
-                labelId='select-a-guide'
-                onChange={handleChange}
-                value={guide}
-                IconComponent={ExpandMoreRoundedIcon}
-              >
-                <MenuItem value=''><em>Select a Guide</em></MenuItem>
-                <MenuItem value='colors'>Colors</MenuItem>
-                <MenuItem value='palette'>Type Standards</MenuItem>
-                <MenuItem value='typography'>Typography</MenuItem>
-              </Select>
-            </FormControl>
-          </Grid>
-          <Grid item xs={10} className={classes.gridItem1}>
-            <Guide />
-          </Grid>
+    <Container className={classes.container}>
+      <Grid container spacing={2}>
+        <Grid item xs={12} sm={2} className={classes.gridItem0}>
+          <FormControl>
+            <InputLabel id='select-a-guide'>Guides</InputLabel>
+            <Select autoWidth labelId='select-a-guide' onChange={handleChange} value={guide} IconComponent={ExpandMoreRoundedIcon}>
+              <MenuItem value=''>
+                <em>Select a Guide</em>
+              </MenuItem>
+              <MenuItem value='buttons'>Buttons</MenuItem>
+              <MenuItem value='colors'>Colors</MenuItem>
+              <MenuItem value='type-standards'>Type Standards</MenuItem>
+              <MenuItem value='typography'>Typography</MenuItem>
+            </Select>
+          </FormControl>
         </Grid>
-      </Container>
-      <Footer />
-    </>
-  )
-}
+        <Grid item xs={12} sm={10} className={classes.gridItem1}>
+          <Guide />
+        </Grid>
+      </Grid>
+    </Container>
+  );
+};
 
-export default Guides
+export default withRouter(Guides);
