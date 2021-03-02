ccf51f23e88b04f2bafa486f662b3dcee146cd1e
Bruce Lai <bruceplai@yahoo.com>
2021-03-01 23:20:48 -0800

    Refactor donation page with Material UI
    * Add Grid components
    
    * Reduce style specific to donation page

--- src/pages/Donation/index.js
+++ src/pages/Donation/index.js
@@ -1,11 +1,8 @@
 import React from 'react';
-import Box from '@material-ui/core/Box';
-import Container from '@material-ui/core/Container';
-import Typography from '@material-ui/core/Typography';
-import Link from '@material-ui/core/Link';
+import { Grid, Container, Typography } from '@material-ui/core';
 
 import BottomCallToAction from '../../components/BottomCallToAction';
-import BreadCrumbs from "../../components/NavBreadcrumbs.js";
+import NavBreadCrumbs from "../../components/NavBreadcrumbs.js";
 
 import useStyles from './styles.js';
 
@@ -21,39 +18,47 @@ const Donation = () => {
 
   return (
     <Container className="containerGray">
-      <Container className={classes.commonSection + ' ' + classes.titleSection}>
-        <BreadCrumbs crumbs={breadCrumbLinks} />
-        <Typography variant='h2' className={classes.donateTitle}>Make a Donation</Typography>
-      </Container>
+      <Grid container spacing={2}>
+        <Grid item xs={false} sm={1} />
+        <Grid item xs={12} sm={10}>
+          <NavBreadCrumbs crumbs={breadCrumbLinks} />
+        </Grid>
+        <Grid item xs={false} sm={1} />
+        <Grid item xs={false} sm={1} />
+        <Grid container item direction='column' spacing={8} xs={12} sm={5} >
+          <Grid item>
+            <Typography variant='h2'>Make a Donation</Typography>
+          </Grid>
+          <Grid item>
+            <Typography variant='body1' className={classes.infoDescription}>
+              The Civic Tech Index is being developed by Hack For LA,
+              and is currently taking donations through Code For America.
+            </Typography>
 
-      <Container className={classes.commonSection + ' ' + classes.infoSection}>
-        <Box className={classes.infoText}>
-          <Typography variant='body1' className={classes.infoDescription}>
-            The Civic Tech Index is being developed by Hack For LA,
-            and is currently taking donations through Code For America.
-          </Typography>
+            <Typography variant='body1' className={classes.infoSteps}>
+              1. Please follow this link to the Code For America{' '}
+              <a href='https://www.codeforamerica.org/donate'>donation form.</a>
+            </Typography>
 
-          <Typography variant='body1' className={classes.infoSteps}>
-            1. Please follow this link to the Code For America{' '}
-            <a
-              className={classes.styledLinkMobile + ' ' + classes.styledLink}
-              href='https://www.codeforamerica.org/donate'>
-              donation form.
-            </a>
-          </Typography>
-
-          <Typography variant='body1' className={classes.infoSteps}>
-            2. To make a donation to the Civic Tech Index, please
-            select <strong>Hack For LA</strong> under the brigades listed.
-          </Typography>
-        </Box>
-        <img
-          alt='A gif demonstrating the use of the donation form'
-          className={classes.infoGif}
-          src='./images/CTI-Donate-to-HfLA.gif' />
-
-        <Typography variant='body1' className={classes.infoThank}>We appreciate your contribution.</Typography>
-      </Container>
+            <Typography variant='body1' className={classes.infoSteps}>
+              2. To make a donation to the Civic Tech Index, please
+              select <strong>Hack For LA</strong> under the brigades listed.
+            </Typography>
+          </Grid>
+        </Grid>
+        <Grid container item direction='column' xs={12} sm={5} >
+          <Grid item>
+            <img
+              alt='A gif demonstrating the use of the donation form'
+              className={classes.infoGif}
+              src='./images/CTI-Donate-to-HfLA.gif' />
+          </Grid>
+        </Grid>
+        <Grid item xs={false} sm={1} />
+        <Grid item xs={12}>
+          <Typography variant='body1' className={classes.infoThank}>We appreciate your contribution.</Typography>
+        </Grid>
+      </Grid>
       <BottomCallToAction heading='Want to support in other ways?' />
     </Container>
   );
