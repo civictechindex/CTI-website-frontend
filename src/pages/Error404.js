72e3e410291aad51cf1c90d2ac76b7a5810acb95
Brent Hengeveld <jafar.ironclad@gmail.com>
2021-03-11 17:04:51 -0800

    added wip 404 page and error 404 routing

---
+++ src/pages/Error404.js
@@ -0,0 +1,28 @@
+import React from 'react';
+import { Box, Card, CardContent, Container, Typography } from '@material-ui/core';
+import { TitleSection } from '../components';
+import ErrorOutlineRoundedIcon from '@material-ui/icons/ErrorOutlineRounded';
+
+const defaultStyle = {
+  backgroundColor: '#FFFFFF',
+  width: '70%',
+}
+
+const Error404 = () => {
+  // No breadcrumbs on Figma design
+
+  return (
+    <Box className='containerDefault'>
+      <Container maxWidth='lg' >
+        <Card className='card244'>
+          <CardContent>
+            <TitleSection>404 <ErrorOutlineRoundedIcon /></TitleSection>
+            <Typography variant='h3'>Oops! The page you are looking for has moved or does not exist.</Typography>
+          </CardContent>
+        </Card>
+      </Container>
+    </Box>
+  );
+};
+
+export default Error404;
