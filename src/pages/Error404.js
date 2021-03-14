eb2c2f41e512919c4c6c4f958b024f7b3b7d362e
Brent Hengeveld <jafar.ironclad@gmail.com>
2021-03-14 13:53:30 -0700

    updated error 404 page for formatting. added cypress test spec. closes #370

--- src/pages/Error404.js
+++ src/pages/Error404.js
@@ -1,26 +1,17 @@
 import React from 'react';
 import { Box, Card, CardContent, Container, Typography } from '@material-ui/core';
-import { TitleSection } from '../components';
 import ErrorOutlineRoundedIcon from '@material-ui/icons/ErrorOutlineRounded';
 
-<<<<<<< HEAD
-const defaultStyle = {
-  backgroundColor: '#FFFFFF',
-  width: '70%',
-}
-
-=======
->>>>>>> 4280579465063bed4d7c21a13ee7669558cbc463
 const Error404 = () => {
   // No breadcrumbs on Figma design
 
   return (
-    <Box className='containerDefault'>
-      <Container maxWidth='lg' >
-        <Card className='card244'>
+    <Box className='containerDefault' p={10}>
+      <Container maxWidth='sm' >
+        <Card className='card241'>
           <CardContent>
-            <TitleSection>404 <ErrorOutlineRoundedIcon /></TitleSection>
-            <Typography variant='h3'>Oops! The page you are looking for has moved or does not exist.</Typography>
+            <Typography variant='h1' color='textPrimary'>404 <ErrorOutlineRoundedIcon /> </Typography>
+            <Typography display="block" variant='h4' color='textSecondary'>Oops! The page you are looking for has moved or does not exist.</Typography>
           </CardContent>
         </Card>
       </Container>
