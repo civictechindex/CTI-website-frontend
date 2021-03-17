a271ed1832dfe1a339a62bfc5b653c8481d62a3b
Bruce Lai <bruceplai@yahoo.com>
2021-03-17 11:24:02 -0700

    Update ParentContributor styling to match Figma (#358)
    * Simplify DOM and replace elements with Material UI components
    
    * Reduce the amount of codeForAll CSS

--- src/components/ParentContributor.js
+++ src/components/ParentContributor.js
@@ -1,43 +1,37 @@
 import React from "react";
 // import logo from '../../public/images/Brigade-Photo.png'
 import { useStyle } from "../pages/Contributors/styles";
+import Box from '@material-ui/core/Box';
+import Typography from '@material-ui/core/Typography';
 
-export const ParentContributor = ({ dropdownLength, children }) => {
+export const ParentContributor = ({ dropdownLength, children }) => {  
   const classes = useStyle();
 
   return (
     <>
-      <div className={classes.codeForAllThumbnailWrapper}>
+      <Box className={classes.codeForAllWrapper}>
         <a
           href={"https://codeforall.org"}
-          className={classes.codeForAllThumbnails}
           target="_blank"
           rel="noreferrer noopener"
         >
-          <div className={classes.codeForAllImageWrapper}>
-            <img
-              src={"https://codeforall.org/assets/images/homepage/logo.png"}
-              className={classes.codeForAllThumbnailImage}
-              onError={(e) =>
-                // eslint-disable-next-line no-console
-                console.log(
-                  e,
-                  `error with 'Code For All' ParentContributor Component`
-                )
-                // Before MVP: Refactor as on-website error message / generic case.
-              }
-              alt="code for all logo"
-              loading="lazy"
-            />
-          </div>
-          <div className={classes.codeForAllTextWrapper}>
-            <p className={classes.codeForAllThumbnailText}>Code For All</p>
-          </div>
+          <img
+            src={"https://codeforall.org/assets/images/homepage/logo.png"}
+            onError={(e) =>
+              // eslint-disable-next-line no-console
+              console.log(
+                e,
+                `error with 'Code For All' ParentContributor Component`
+              )
+              // Before MVP: Refactor as on-website error message / generic case.
+            }
+            alt="code for all logo"
+            loading="lazy"
+          />
+          <Typography variant='body2'>Code For All</Typography>
         </a>
-        <h5 className={classes.dropdownLength}>
-          <span>({dropdownLength})</span>
-        </h5>
-      </div>
+        <Typography variant='body2'>({dropdownLength})</Typography>
+      </Box>
       {children}
     </>
   );
