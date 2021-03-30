e3b240dd3e4912e164dc770487c5e1857927fe1d
e0marina <emilymarinaro@gmail.com>
2021-03-30 15:58:10 -0700

    easily my finest work

--- src/components/PhotoCardDsktp.js
+++ src/components/PhotoCardDsktp.js
@@ -42,30 +42,35 @@ function PhotoCardDsktp(props) {
                     <Grid item style={{ margin: 'auto' }} align='center' >
                         <Card className={classes.root} style={{ position: 'relative', padding: '0px', marginBottom: '5px', width: '1184px' }}>
                             <Paper elevation={0} style={styles.cardContainer}>
-                                <Typography className={classes.cardHeading} style={{ position: 'absolute', left: '69px', top: '94px', fontSize: '42px', lineHeight: '48px' }} gutterBottom variant="h4">
-                                    {i.heading}
-                                </Typography>
-                                <Typography variant="h6" className={classes.cardParagraphHeading} style={{
-                                    position: 'absolute', left: '69px', top: '159px'
-                                }}>
-                                    {i.subHeading}
-                                </Typography>
-                                <br></br>
-                                <Typography style={{ position: 'absolute', left: '69px', top: '197px' }}>
-                                    {i.dsktpTxtLn1}
-                                </Typography>
-                                <Typography style={{ position: 'absolute', left: '69px', top: '220px' }}>
-                                    {i.dsktpTxtLn2}
-                                </Typography>
-                                <Typography style={{ position: 'absolute', left: '69px', top: '243px' }}>
-                                    {i.dsktpTxtLn3}
-                                </Typography>
-                                <Typography variant="h6" style={{ position: 'absolute', left: '69px', top: '280px' }} >
-                                    {i.footerText}
-                                </Typography>
-                                <Button href={styles.donatePath} style={{ border: '2px solid black', backgroundColor: 'white', borderColor: '#0F1D2F', color: 'black', position: 'absolute', left: '60px', top: '330px', width: '225px', margin: '0 auto', padding: '16px' }}>
-                                    {i.buttonText}
-                                </Button>
+                                <Grid item>
+                                    <Typography className={classes.cardHeading} style={{ position: 'absolute', left: '69px', top: '94px', fontSize: '42px', lineHeight: '48px' }} gutterBottom variant="h4">
+                                        {i.heading}
+                                    </Typography>
+                                    <Typography variant="h6" className={classes.cardParagraphHeading} style={{
+                                        position: 'absolute', left: '69px', top: '159px'
+                                    }}>
+                                        {i.subHeading}
+                                    </Typography>
+                                    <br></br>
+                                    <Typography style={{ position: 'absolute', left: '69px', top: '197px' }}>
+                                        {i.dsktpTxtLn1}
+                                    </Typography>
+                                    <Typography style={{ position: 'absolute', left: '69px', top: '220px' }}>
+                                        {i.dsktpTxtLn2}
+                                    </Typography>
+                                    <Typography style={{ position: 'absolute', left: '69px', top: '243px' }}>
+                                        {i.dsktpTxtLn3}
+                                    </Typography>
+                                    <Typography variant="h6" style={{ position: 'absolute', left: '69px', top: '280px' }} >
+                                        {i.footerText}
+                                    </Typography>
+                                    <Button href={styles.donatePath} style={{ border: '2px solid black', backgroundColor: 'white', borderColor: '#0F1D2F', color: 'black', position: 'absolute', left: '60px', top: '330px', width: '225px', margin: '0 auto', padding: '16px' }}>
+                                        {i.buttonText}
+                                    </Button>
+                                </Grid>
+                                <Grid item>
+                                    <img src={i.src} style={{ width: '544px', height: '382px', position: 'absolute' }} alt='people smiling about computers'></img>
+                                </Grid>
                             </Paper>
                         </Card>
                     </Grid>
