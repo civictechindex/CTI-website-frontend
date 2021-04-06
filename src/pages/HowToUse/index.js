519797fc6724df33df56dfb3e9ce2c2e633dacfc
Bhaggyalakshmi <bhaggya@gmail.com>
2021-04-05 23:40:56 -0700

    Adding projects to the index-Styling

--- src/pages/HowToUse/index.js
+++ src/pages/HowToUse/index.js
@@ -1,45 +1,147 @@
 import React from 'react'
-import { Link } from 'react-router-dom'
-import Container from "@material-ui/core/Container";
-import BottomCallToAction from '../../components/BottomCallToAction'
-import useStyles from './styles.js'
+import NavBreadcrumbs from '../../components/NavBreadcrumbs'
+import Grid from '@material-ui/core/Grid';
+import Typography from '@material-ui/core/Typography';
+import Button from '@material-ui/core/Button';
+import Box from '@material-ui/core/Box';
+import { List } from '@material-ui/core';
+import ListItem from '@material-ui/core/ListItem';
+import ListItemText from '@material-ui/core/ListItemText';
+import { makeStyles } from '@material-ui/core/styles';
+
+const useStyles = makeStyles(theme => ({
+  tagText: {
+    marginTop: '20px',
+    paddingBottom: '30px',
+  },
+  projectsSubHeader: {
+    backgroundColor: theme.palette.background.primary,
+    textAlign: 'center',
+  },
+  projectsList: {
+    color: theme.palette.text.primary,
+    paddingLeft: '126px',
+    backgroundColor: theme.palette.background.secondary,
+  },
+  btnText: {
+    backgroundColor: '#0CB2E7',
+    borderRadius: '12px',
+  },
+  btnStyle: {
+    width: '468px',
+    height: '56px',
+    paddingTop: '13px',
+    paddingLeft: '25rem',
+  },
+  button: {
+    borderRadius: '5em',
+    backgroundColor: '#fff',
+    color: '#6D6E74',
+    fontSize: '14px',
+    fontFamily: 'Work Sans',
+    '&:hover': {
+      backgroundColor: '#fff',
+    },
+  },
+  projectText: {
+    fontSize: '32px',
+    lineHeight: '40px',
+    fontWeight: 'bold',
+    textAlign: 'center',
+    paddingTop: '40px',
+    color: '#042D5F',
+  },
+  projText: {
+    marginTop: '40px',
+    paddingLeft: '21rem',
+    paddingBottom: '64px',
+  },
+
+  boxItem: {
+    border: '1px solid #BCBCBC',
+    borderRadius: '15px',
+    fontSize: '18px',
+    width: '272px',
+    height: '152px',
+  },
+  boxText: {
+    paddingTop: '30px',
+    paddingLeft: '37px',
+  },
+
+}));
+
+
+const crumbs = [{ href: '/About ', name: 'About ' }, { href: '/adding-Projects', name: 'Adding Projects to the Index' }]
+
+const TitleSection = () => {
+  const classes = useStyles()
+  return (
+    <Grid container>
+      <Grid item xs={12}><Typography color='textSecondary'>Civic Tech Index</Typography></Grid>
+      <Grid item xs={12}><Typography variant='h2' color='textPrimary'>Tag Generator</Typography>
+        <Typography variant='body1' color='textSecondary' className={classes.tagText}>Join the Civic Tech Index by submitting your open-source project.<br /> This process takes less than one minute to complete.</Typography>
+      </Grid>
+    </Grid >
+  )
+}
 
 const HowToUse = () => {
   const classes = useStyles()
 
   return (
-    <Container className='containerGray'>
-      <p className={classes.projectsLink}>About / Adding Projects to the Index</p>
-      <h1 className={classes.projectsHeading}>Adding Projects to the Index</h1>
-      <h6 className={classes.projectsSubHeading}>Generating Topics/Tags for your Civic Tech Project</h6>
-      <p className={classes.projectsSubHeadingDetails}>We recommend having your project&apos;s repository open in another browser for ease of convenience</p>
-      <ol className={classes.projectsList}>
-        <li>On the Civic Tech Index <Link to='/' className={classes.link}>Tag Generator,</Link> navigate to the form and complete the following fields</li>
-        <textarea></textarea>
-        <li>To create tags, type in keywords that describe your topic(s), causes(s), or civic issue(s) that your project addresses.</li>
-        <textarea></textarea>
-        <li>Copy the tages that appear below each keyword.</li>
-        <textarea></textarea>
-        <li>Navigate to your project&apos;s repository in another browser to add your generated tags.</li>
-        <li>Under your project&apos;s repository, click <span className={classes.bold}>Manage Topics</span> to paste your tags.</li>
-        <div className={classes.imgContainer}>
-          <img className={classes.img} src='/images/how-to-use-step-1.png' alt='first step on how to manage your tags in github' />
-          <p className={`${classes.imgText} ${classes.gray}`}>civictechindex <span className={classes.black}>/</span> <strong>civictechindex-launch</strong></p>
-        </div>
-        <div className={classes.imgContainer}>
-          <img className={classes.img} src='/images/how-to-use-step-2.png' alt='second step on how to manage your tags in github' />
-          <p className={`${classes.imgText} ${classes.gray}`}>civictechindex <span className={classes.black}>/</span> <strong>civictechindex-launch</strong></p>
-        </div>
-        <li>Repeat until you have finished adding all of your tags, then click <span className={classes.bold}>Done</span> to save your new topics.</li>
-        <div className={classes.imgContainer}>
-          <img className={classes.img} src='/images/how-to-use-step-3.png' alt='last step on how to manage your tags in gitbub' />
-          <p className={classes.imgText}>civictechindex <span className={classes.black}>/</span> <strong>civictechindex-launch</strong></p>
-        </div>
-      </ol>
-      <p className={classes.projectsFooter}>Don&apos;t forget to tell us when you&apos;ve submitted a project or if you would like your organization added to our network!</p>
-      <BottomCallToAction heading='Submitted a project or still need help?' />
-    </Container>
+    <>
+      <Grid className={classes.projectsSubHeader}>
+        <NavBreadcrumbs crumbs={crumbs} color='textPrimary' />
+        <TitleSection />
+      </Grid>
+      <Typography className={classes.projectsList}>
+        <List>
+          <ListItem alignItems="flex-start" >
+            <ListItemText primary={
+              <Typography style={{ fontSize: '17px', paddingBottom: '12px' }}> 1. Navigate to your project&apos;s repository in another browser to add your generated tags.</Typography>}>
+            </ListItemText>
+          </ListItem>
+          <ListItem alignItems="flex-start">
+            <ListItemText primary={
+              <Typography style={{ fontSize: '17px' }}>2. Under your project’s repository, click to paste your tags.</Typography>}>
+            </ListItemText>
+            <ListItemText><img src="/images/step_2.png" alt="Step 2" /></ListItemText>
+          </ListItem>
+          <ListItem alignItems="flex-start">
+            <ListItemText primary={
+              <Typography style={{ fontSize: '17px' }}>3. Under &quot;Topics&quot;, paste the topic you want to add to your repository.</Typography>}>
+            </ListItemText>
+            <ListItemText><img src="/images/step_3.png" alt="Step 3" /></ListItemText>
+          </ListItem>
+          <ListItem alignItems="flex-start">
+            <ListItemText primary={
+              <Typography style={{ fontSize: '17px' }}>4. Repeat until you have finished adding all of your tags, then click Save Changes.</Typography>}>
+            </ListItemText>
+            <ListItemText><img src="/images/step_4.png" alt="Step 4" /></ListItemText>
+          </ListItem>
+        </List>
+      </Typography>
+      <Grid>
+        <Grid className={classes.projectText}> This project is so new we are celebrating every win</Grid>
+      </Grid>
+      <Box className={classes.btnStyle}>
+        <Button className={classes.btnText}>Let us know when you&apos;ve added  #civictechindex</Button>
+      </Box>
+      <Grid container direction="row" className={classes.projText}>
+        <Box component="div" className={classes.boxItem} p={1} m={1}>
+          <Typography className={classes.boxText}>Add Another Project</Typography>
+          <Grid className={classes.boxText}> <Button variant="outlined" className={classes.button} href="/tag-generator">Tag Generator</Button></Grid>
+        </Box>
+        <Box component="div" container direction="row" className={classes.boxItem} p={1} m={1}>
+          <Typography className={classes.boxText}>Collaborate with us</Typography>
+          <Grid className={classes.boxText}> <Button variant="outlined" className={classes.button} href="/radicalcollaboration/faq">Learn More</Button></Grid>
+        </Box>
+      </Grid>
+    </>
   )
 }
 
 export default HowToUse
+
+
