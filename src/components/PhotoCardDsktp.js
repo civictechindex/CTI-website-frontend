7027fb9791b65ee8e8f68223281daf2f030f3e77
e0marina <emilymarinaro@gmail.com>
2021-04-06 19:29:28 -0700

    part of making this modular

--- src/components/PhotoCardDsktp.js
+++ src/components/PhotoCardDsktp.js
@@ -1,8 +1,10 @@
 import React from 'react';
-import CardOne from './Card_One_PCDsktp'
+import CardOne from './CardOnePCDsktp'
+import CardTwo from './CardTwoPCDsktp'
+import CardThree from './CardThreePCDsktp'
+import CardFour from './CardFourPCDsktp'
 //todo: (1)add links to 3rd and 4th cards. 
 //(2)Make sure diff views (mobile) display properly
-//(3)Probably have to break out code so not so many lines in this file
 //(4)Make sure correct buttons linked to the right pages
 
 
@@ -10,6 +12,10 @@ function PhotoCardDsktp(props) {
     return (
         <>
             <CardOne items={props.items} />
+            <CardTwo items={props.items} />
+            <CardThree items={props.items} />
+            <CardFour items={props.items} />
+
         </>
     );
 }
