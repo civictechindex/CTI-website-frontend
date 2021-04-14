10d98bbbaa5854b03a79a84af5a61c1f6ab60a30
Bhaggyalakshmi <bhaggya@gmail.com>
2021-04-14 15:56:51 -0700

    Stying Donate page as per Figma

--- src/pages/Donation/index.js
+++ src/pages/Donation/index.js
@@ -3,64 +3,50 @@ import Grid from '@material-ui/core/Grid';
 import Container from '@material-ui/core/Container';
 import Typography from '@material-ui/core/Typography';
 import BottomCallToAction from '../../components/BottomCallToAction';
-import NavBreadcrumbs from "../../components/NavBreadcrumbs.js";
 import useStyles from './styles.js';
-
-const breadCrumbLinks = [
-  { href: '/home', name: 'Home' },
-  { href: '/donate#root', name: 'Radical Collaboration' }, // Update when page is built
-  { href: '/donate', name: 'Support Us' },
-  { href: '/donate', name: 'Donate' },
-];
+import { HeaderSection } from './HeaderSection'
+import Box from '@material-ui/core/Box'
 
 const Donation = () => {
   const classes = useStyles();
 
   return (
-    <Container className="containerGray">
-      <Grid container spacing={2}>
-        <Grid item xs={false} sm={1} />
-        <Grid item xs={12} sm={10}>
-          <NavBreadcrumbs crumbs={breadCrumbLinks} color="secondary"/>
-        </Grid>
-        <Grid item xs={false} sm={1} />
-        <Grid item xs={false} sm={1} />
-        <Grid container item direction='column' spacing={8} xs={12} sm={5} >
-          <Grid item>
-            <Typography variant='h2'>Make a Donation</Typography>
-          </Grid>
-          <Grid item>
-            <Typography variant='body1' className={classes.infoDescription}>
-              The Civic Tech Index is being developed by Hack For LA,
-              and is currently taking donations through Code For America.
-            </Typography>
-
-            <Typography variant='body1' className={classes.infoSteps}>
-              1. Please follow this link to the Code For America{' '}
-              <a href='https://www.codeforamerica.org/donate'>donation form.</a>
-            </Typography>
-
-            <Typography variant='body1' className={classes.infoSteps}>
-              2. To make a donation to the Civic Tech Index, please
+    <Box xs={12} sm={6} md={3}>
+      <HeaderSection />
+      <Container className="containerGray" style={{ paddingBottom: '46px' }}>
+        <Grid container spacing={2}>
+          <Grid container item direction='column' spacing={8} xs={12} sm={5} >
+            <Grid item style={{ paddingTop: '14vw', paddingLeft: '10vw' }}>
+              <Typography variant='body1' className={classes.infoDescription}>
+                The Civic Tech Index is being developed by Hack For LA,
+                and is currently taking donations through Code For America.
+              </Typography>
+              <Typography variant='body1' className={classes.infoSteps}>
+                1. Please follow this link to the Code For America{' '}
+                <a href='https://www.codeforamerica.org/donate'>donation form.</a>
+              </Typography>
+              <Typography variant='body1' className={classes.infoSteps}>
+                2. To make a donation to the Civic Tech Index, please
               select <strong>Hack For LA</strong> under the brigades listed.
-            </Typography>
+              </Typography>
+              <Typography variant='h4' className={classes.infoThank}>We appreciate your contribution.</Typography>
+            </Grid>
           </Grid>
-        </Grid>
-        <Grid container item direction='column' xs={12} sm={5} >
-          <Grid item>
-            <img
-              alt='A gif demonstrating the use of the donation form'
-              className={classes.infoGif}
-              src='./images/CTI-Donate-to-HfLA.gif' />
+          <Grid container item direction='column' xs={12} sm={5} >
+            <Grid item>
+              <img
+                alt='A gif demonstrating the use of the donation form'
+                className={classes.infoGif}
+                src='./images/CTI-Donate-to-HfLA.gif' />
+            </Grid>
           </Grid>
+          <Grid item xs={false} sm={1} />
         </Grid>
-        <Grid item xs={false} sm={1} />
-        <Grid item xs={12}>
-          <Typography variant='h4' className={classes.infoThank}>We appreciate your contribution.</Typography>
-        </Grid>
-      </Grid>
-      <BottomCallToAction heading='Want to support in other ways?' />
-    </Container>
+      </Container>
+      <Container className={classes.contactUs} >
+        <BottomCallToAction heading='Want to support in other ways?' />
+      </Container>
+    </Box>
   );
 }
 
--- src/pages/Donation/styles.js
+++ src/pages/Donation/styles.js
@@ -2,22 +2,71 @@ import makeStyles from '@material-ui/core/styles/makeStyles'
 
 const useStyles = makeStyles(theme => ({
   infoDescription: {
-    fontSize: '20px',
+    color: '#004364',
+    fontFamily: 'Work Sans',
+    fontWeight: 'bold',
+    fontSize: '24px',
+    width: '80vw',
+    [theme.breakpoints.up('md')]: {
+      fontSize: '18px',
+    },
+    [theme.breakpoints.up('sm')]: {
+      width: '39vw',
+      fontSize: '24px',
+    },
   },
 
   infoGif: {
     boxShadow: '0 0px 0px 0 rgba(0, 0, 0, 0.2), 0 0px 20px 0 rgba(0, 0, 0, 0.19)',
-    width: '100%',
-    marginTop: '5vw',
+    marginTop: '4vw',
+    marginLeft: '19vw',
+    [theme.breakpoints.down('md')]: {
+      width: '43vw',
+      height: '43vw',
+    },
+    [theme.breakpoints.up('lg')]: {
+      width: '535px',
+      height: '466px',
+      marginLeft: '16vw',
+    },
   },
 
   infoSteps: {
-    fontSize: '18px',
+
     marginTop: '2.5vw',
+    marginBottom: '3vw',
+    marginLeft: '10px',
+    [theme.breakpoints.up('md')]: {
+      fontSize: '16px',
+    },
+    [theme.breakpoints.up('sm')]: {
+      fontSize: '16px',
+    },
+    [theme.breakpoints.up('lg')]: {
+      fontSize: '18px',
+    },
   },
 
   infoThank: {
     textAlign: 'center',
+    color: '#004364',
+    [theme.breakpoints.down('md')]: {
+      fontSize: '15px',
+    },
+    [theme.breakpoints.up('lg')]: {
+      fontSize: '20px',
+    },
+  },
+
+  contactUs: {
+    backgroundColor: 'theme.palette.background.secondary',
+    overflow: 'hidden',
+    '& h2': {
+      [theme.breakpoints.down('sm')]: {
+        fontSize: '1.25rem',
+        overflow: 'hidden',
+      },
+    },
   },
 
 }))
