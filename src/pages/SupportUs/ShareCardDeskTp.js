f233edb30baf09138a746d455e7433cfb6aebcc6
e0marina <emilymarinaro@gmail.com>
2021-04-19 15:54:42 -0700

    updating linting for sharecard

--- src/pages/SupportUs/ShareCardDeskTp.js
+++ src/pages/SupportUs/ShareCardDeskTp.js
@@ -1,3 +1,4 @@
+/* eslint-disable react/jsx-key */
 import React from 'react';
 import { Grid } from "@material-ui/core";
 import { makeStyles } from '@material-ui/core/styles';
@@ -9,73 +10,73 @@ import Typography from '@material-ui/core/Typography';
 import './styles.css';
 
 const useStyles = makeStyles(
-    (theme) => ({
-        root: {
-            maxWidth: 1280,
-        },
-        cardHeading: {
-            color: theme.palette.secondary.dark
-        },
-        cardParagraphHeading: {
-            color: theme.palette.primary.main
-        },
-        largeButton: {
-            button: theme.overrides.MuiButton,
-            color: theme.palette.primary.main,
-            backgroundColor: theme.palette.background.default,
+  (theme) => ({
+    root: {
+      maxWidth: 1280,
+    },
+    cardHeading: {
+      color: theme.palette.secondary.dark,
+    },
+    cardParagraphHeading: {
+      color: theme.palette.primary.main,
+    },
+    largeButton: {
+      button: theme.overrides.MuiButton,
+      color: theme.palette.primary.main,
+      backgroundColor: theme.palette.background.default,
 
-        }
-    })
+    },
+  })
 );
-function ShareCardDeskTp(props) {
-    const items = props.items;
-    const classes = useStyles();
-    return (
-        <>
-            {items.map((i, idx) => {
-                let card;
-                if (i.title === 'img-2') {
-                    card = <div>
-                        <Grid item>
-                            <Paper elevation={0} id='innerTextCardContainer'>
-                                <Typography className={classes.cardHeading} id='rightCardHeading' gutterBottom variant="h4">
-                                    {i.heading}
-                                </Typography>
-                                <Typography variant="h6" className={classes.cardParagraphHeading} id='rightPHeading'>
-                                    {i.subHeading}
-                                </Typography>
-                                <br></br>
-                                <Typography id='rightDtpLine1'>
-                                    {i.dsktpTxtLn1}
-                                </Typography>
-                                <Typography id='rightDtpLine2'>
-                                    {i.dsktpTxtLn2}
-                                </Typography>
-                                <Button href='/radicalcollaboration/sharethecti' id='buttonRight'>
-                                    {i.buttonText}
-                                </Button>
-                            </Paper>
-                        </Grid>
-                        <Grid item >
-                            <Card className={classes.root} id='imgCardLeft'>
-                                <CardMedia
-                                    id='cardMedia'
-                                    className={classes.media}
-                                    image={i.src}
-                                />
-                            </Card>
-                        </Grid>
-                    </div>
-                }
-                return (
-                    <Grid container id='gridCont' align='center' >
-                        <Card className={classes.root} id='outerCard'>
-                            {card}
-                        </Card>
-                    </Grid>
-                )
-            })}
-        </>
-    );
+const ShareCardDeskTp = (props) => {
+  const items = props.items;
+  const classes = useStyles();
+  return (
+    <>
+      {items.map((i, idx) => {
+        let card;
+        if (i.title === 'img-2') {
+          card = <div>
+            <Grid item>
+              <Paper elevation={0} id='innerTextCardContainer'>
+                <Typography className={classes.cardHeading} id='rightCardHeading' gutterBottom variant="h4">
+                  {i.heading}
+                </Typography>
+                <Typography variant="h6" className={classes.cardParagraphHeading} id='rightPHeading'>
+                  {i.subHeading}
+                </Typography>
+                <br></br>
+                <Typography id='rightDtpLine1'>
+                  {i.dsktpTxtLn1}
+                </Typography>
+                <Typography id='rightDtpLine2'>
+                  {i.dsktpTxtLn2}
+                </Typography>
+                <Button href='/radicalcollaboration/sharethecti' id='buttonRight'>
+                  {i.buttonText}
+                </Button>
+              </Paper>
+            </Grid>
+            <Grid item >
+              <Card className={classes.root} id='imgCardLeft'>
+                <CardMedia
+                  id='cardMedia'
+                  className={classes.media}
+                  image={i.src}
+                />
+              </Card>
+            </Grid>
+          </div>
+        }
+        return (
+          <Grid container id='gridCont' align='center' >
+            <Card className={classes.root} id='outerCard'>
+              {card}
+            </Card>
+          </Grid>
+        )
+      })}
+    </>
+  );
 }
 export default ShareCardDeskTp;
