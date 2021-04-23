3dc6d59bd1ade9cf626dd656a45cbed00fe1f03a
Maxwell Countryman Skewes <maxskewes@gmail.com>
2021-04-22 21:09:44 -0700

    Updated Footer
    Fixed spacing issues with proper display on 3 screen types, created annotation component in-line with figma, added spacing between email text input and submit button but could not size them properly - I will create a new issue for this.

--- src/components/Footer/linkList.js
+++ src/components/Footer/linkList.js
@@ -1,15 +1,19 @@
 import React from 'react';
 import { Link } from 'react-router-dom/';
 import Typography from '@material-ui/core/Typography';
+import useStyles from './styles';
 
 const LinkList = ({ header, links }) => {
+  const classes = useStyles();
 
   return (
     <>
-      <Typography variant='body2' color='textSecondary'>{header}</Typography>
-      {links.map((link) => {
-        return link.external ? <a href={link.to}>{link.text}</a> : <Link to={link.to}>{link.text}</Link>
-      })}
+      <div className={classes.containerItem}>
+        <Typography style={{ whiteSpace: 'nowrap' }} variant='body2' color='textSecondary'>{header}</Typography>
+        {links.map((link) => {
+          return link.external ? <a href={link.to}>{link.text}</a> : <Link to={link.to}>{link.text}</Link>
+        })}
+      </div>
     </>
   );
 };
