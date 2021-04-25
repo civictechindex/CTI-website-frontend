70491943836e17d3f833ec77ca8bdfff8ebf1832
e0marina <30880308+e0marina@users.noreply.github.com>
2021-04-25 14:43:04 -0700

    Support us page317 (#467)
    Merged as agreed during meeting on 4/25
    
    * updating Support page per UI theme
    
    * Adding card header and subheader
    
    * Mobile Card typography updating to MUI theme
    
    * button updated for support us page
    
    * added images
    
    * updated button color
    
    * updated text for all cards using passing props
    
    * updated some text
    
    * splitting photocard into two components, one for mobile, one desktop
    
    * updating PhotoCard Desktop styling
    
    * changed some styles
    
    * added smaller image to work on desktop version
    
    * updated photocarddsktp component with gradient background images
    
    * updating desktop card design
    
    * -
    
    * working on button
    
    * button styling looks like figma
    
    * easily my finest work
    
    * working on the images
    
    * images now card background images
    
    * updated some styling
    
    * started conditional rendering for image/content switching on cards
    
    * 2nd card rendering properly for desktop now
    
    * fourth card at bottom looking pretty good
    
    * added some to do notes
    
    * added first card as its own component
    
    * modularized the different cards so not so many lines of code
    
    * part of making this modular
    
    * added correct links to buttons and put highlighting on linked text
    
    * added links to hrefs for inline links on cards
    
    * added correct colors for links inline on cards
    
    * added some small styling before push
    
    * resolved conflict in components/index.js file
    
    * added back ProjectCard into index.js
    
    * moved Support Us specific components under pages/SupportUs
    
    * renamed cards in support us page to make more sense
    
    * made a styles.css file for the support us components and updated styles to be in that file for the cards
    
    * changed volunteer w us link
    
    * enabled eslint and fixed errors
    
    * updating linting for sharecard
    
    * fixed the disappearing cards on breakpoints
    
    * updated FAQCardDeskTp.js to use makeStyles js file
    
    * updated file names
    
    * desktop cards now using useStyles
    
    * added some margin under buttons in mobile view
    
    Co-authored-by: Olivia Chiong <75456721+Olivia-Chiong@users.noreply.github.com>
    Co-authored-by: Kevin Ashworth <kevinashworth@yahoo.com>

---
+++ src/pages/SupportUs/index.js
@@ -0,0 +1,87 @@
+import React from 'react'
+import { Container } from "@material-ui/core";
+import Hidden from '@material-ui/core/Hidden';
+import withWidth from '@material-ui/core/withWidth';
+import PhotoCardDesktop from './PhotoCardDesktop';
+import PhotoCardMobile from './PhotoCardMobile';
+import {
+  GetStartedCard,
+  NavBreadcrumbs,
+  TitleSection,
+} from '../../components';
+
+const SupportUs = () => {
+  const crumbs = [
+    { name: "Home", href: "/home" },
+    { name: "Support Us", href: "/support" },
+  ];
+
+  const Images = [
+    {
+      src: "/images/dsktp-photo-of-people-standing-near-window-3184427.png", alt: "team high fiving",
+      title: "img-1",
+      heading: 'Donate',
+      subHeading: 'Every gift helps us continue our work.',
+      textBody: 'Your tax-deductible gift today will help make more open-source solutions easily available for the communities who need it the most.',
+      footerText: 'Thank You.',
+      buttonText: 'Make a Donation',
+      dsktpTxtLn1: 'Your tax-deductible gift today will help make more',
+      dsktpTxtLn2: 'open-source solutions easily available for the',
+      dsktpTxtLn3: 'communities who need it the most.',
+    },
+    {
+      src: "/images/group-smiling-working.png", alt: "group working",
+      title: "img-2",
+      heading: 'Share the CTI',
+      subHeading: 'Love the Index? Be an evangelist!',
+      textBody: ' Help others and their communities discover, share, and benefit from contributed projects on the Index.',
+      buttonText: 'Share the Civic Tech Index',
+      dsktpTxtLn1: 'Help others and their communities discover, share, and',
+      dsktpTxtLn2: 'benefit from contributed projects on the index.',
+    },
+    {
+      src: "/images/bulletin-board-postits.png", alt: "bulletin board",
+      title: "img-3",
+      heading: 'Volunteer with Us',
+      subHeading: 'Help us improve the Civic Tech Index',
+      textBody: 'Please complete this application if you would like us to reach out to you directly or find us on our project team page.',
+      buttonText: 'Become a Volunteer',
+      dsktpTxtLn1: 'Please complete this application if you would like',
+      dsktpTxtLn2: 'us to reach out to you directly or find us on our',
+      dsktpTxtLn3: 'project team page.',
+
+    },
+    {
+      src: "/images/man-and-woman-working.png", alt: "girl and guy coding",
+      title: "img-4",
+      heading: 'Need Help?',
+      subHeading: "Can't find the answer you're looking for?",
+      textBody: "We've shared some of our most frequently asked questions to help you out! View our FAQ to find answers or contact us.",
+      buttonText: 'View the FAQ',
+      dsktpTxtLn1: "We've shared some of our most frequently asked",
+      dsktpTxtLn2: 'questions to help you out!',
+      dsktpTxtLn3: 'View our FAQ to find answers or contact us.',
+    },
+  ];
+  return (
+    <Container className="containerDefault">
+      <NavBreadcrumbs crumbs={crumbs} style={{ fontColor: '#FEFEFE' }} />
+      <TitleSection>
+                Your Help Goes a Long Way
+      </TitleSection>
+      <Hidden mdDown>
+        <PhotoCardDesktop items={Images} />
+      </Hidden>
+      <Hidden lgUp>
+        <PhotoCardMobile items={Images} />
+      </Hidden>
+      <GetStartedCard
+        headerTitle="Want to support in other ways?"
+        buttonText="Contact Us"
+        buttonHref="mailto:civictechindex@hackforla.org"
+      />
+    </Container>
+  );
+}
+
+export default withWidth()(SupportUs);
