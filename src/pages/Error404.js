48037fa820a29749012d727c4ac9ab13431778f0
Brent Hengeveld <jafar.ironclad@gmail.com>
2021-04-25 15:07:17 -0700

    Error404 medium small format #482 (#513)
    merged as discussed during meeting 4/25
    
    * bypass eslint for console statements on two files. closes #255. closes #257
    
    * added wip 404 page and error 404 routing
    
    * updated error 404 page for formatting. added cypress test spec. closes #370
    
    * WIP breakpoints and MUI Best Practice polish for Error404.
    
    * updated cypress test spec for 404 page
    
    Co-authored-by: Olivia Chiong <75456721+Olivia-Chiong@users.noreply.github.com>

--- src/pages/Error404.js
+++ src/pages/Error404.js
@@ -1,28 +1,56 @@
 import React from 'react';
 import Box from '@material-ui/core/Box';
-import Card from '@material-ui/core/Card';
-import CardContent from '@material-ui/core/CardContent';
-import Container from '@material-ui/core/Container';
-import Typography from '@material-ui/core/Typography';
+import Grid from '@material-ui/core/Grid'
+import makeStyles from '@material-ui/core/styles/makeStyles';
 import ErrorOutlineRoundedIcon from '@material-ui/icons/ErrorOutlineRounded';
 
 const Error404 = () => {
+
+  const useStyles = makeStyles((theme) => ({
+    errorPageStyle: {
+      textAlign: 'center',
+      '& h1': {
+        [theme.breakpoints.down('sm')]: {
+          color: theme.palette.warning.main,
+        },
+        [theme.breakpoints.up('md')]: {
+          color: theme.palette.warning.main,
+        },
+      },
+      '& h6': {
+        [theme.breakpoints.down('sm')]: {
+          fontSize: '24px',
+          color: theme.palette.text.secondary,
+        },
+        [theme.breakpoints.up('md')]: {
+          fontSize: '24px',
+          color: theme.palette.text.secondary,
+        },
+      },
+    },
+  }));
+
+  const ErrorSection = () => {
+    const classes = useStyles();
+    return (
+      <Grid
+        container
+        justify='center'
+        className={classes.errorPageStyle}
+      >
+        <Grid style={{ fontSize: 52 }}>
+          <h1>404<ErrorOutlineRoundedIcon style={{ fontSize: 84 }}/></h1>
+          <h6>Oops! The page you are looking for has moved or does not exist.</h6>
+        </Grid>
+      </Grid>
+    );
+  };
+
   return (
     <Box className='containerDefault' p={10}>
-      <Container maxWidth='sm'>
-        <Card className='card241'>
-          <CardContent>
-            <Typography variant='h1' color='textPrimary' style={{ fontSize: 128 }}>
-              404 <ErrorOutlineRoundedIcon style={{ fontSize: 96 }} />
-            </Typography>
-            <Typography display='block' variant='h4' color='textSecondary'>
-              Oops! The page you are looking for has moved or does not exist.
-            </Typography>
-          </CardContent>
-        </Card>
-      </Container>
+      <ErrorSection />
     </Box>
-  );
-};
+  )
+}
 
 export default Error404;
