90da2c780f9f8b019230663703e8e31fbc50370d
Bruce Lai <bruceplai@yahoo.com>
2021-04-25 15:24:41 -0700

    Fix useEffect warning in Faq.js (#460)

--- src/pages/RadicalCollaboration/Faq.js
+++ src/pages/RadicalCollaboration/Faq.js
@@ -58,6 +58,7 @@ const Faq = () => {
 
   useEffect(() => {
     getFAQData(query, false);
+  // eslint-disable-next-line react-hooks/exhaustive-deps
   }, [pageNum, largeScreen]);
 
   const handleInput = (event) => {
