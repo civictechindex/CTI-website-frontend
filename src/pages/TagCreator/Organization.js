8a61ab8c946dada320fadde0e0d83cbfbdce4545
nrrao <nandanarao@yahoo.com>
2021-04-26 20:45:19 -0400

    orga list is loading from api

--- src/pages/TagCreator/Organization.js
+++ src/pages/TagCreator/Organization.js
@@ -1,14 +1,46 @@
-import React from 'react';
+import React,{ useState,useEffect } from 'react';
 import axios from 'axios';
 import Button from '@material-ui/core/Button';
 import Grid from '@material-ui/core/Grid';
 import Typography from '@material-ui/core/Typography';
 import TextField from '@material-ui/core/TextField';
 import Autocomplete from '@material-ui/lab/Autocomplete';
-import orgs from './orgs.json';
 import Link from '@material-ui/core/Link';
+import CircularProgress from '@material-ui/core/CircularProgress';
 
 export const OrganizationSelectorSection = ({ orgName, setOrgName }) => {
+  const [open, setOpen] = useState(false);
+  const [options, setOptions] = useState([]);
+  const loading = open && options.length === 0;
+
+  useEffect(() => {
+    let active = true;
+
+    if (!loading) {
+      return undefined;
+    }
+
+    axios.get('https://test-civictechindexadmin.herokuapp.com/api/organizations/')
+      .then(res => {
+        if (active){
+          const orgs = (res.data).map((org) => org.name)
+          orgs.push("")
+          setOptions(orgs);
+        }
+      })
+
+    return () => {
+      active = false;
+    };
+  }, [loading]);
+
+
+  useEffect(() => {
+    if (!open) {
+      setOptions([]);
+    }
+
+  }, [open]);
 
   return (
     <>
@@ -16,12 +48,37 @@ export const OrganizationSelectorSection = ({ orgName, setOrgName }) => {
         <p>Which Organization?</p>
         <Autocomplete
           id="organization"
-          options={orgs}
+          style={{ width: '100%' }}
+          open={open}
+          onOpen={() => {
+            setOpen(true);
+          }}
+          onClose={() => {
+            setOpen(false);
+          }}
+          getOptionSelected={(option, value) => option === value }
+          getOptionLabel={(option) => option}
+          options={options}
+          filterOptions={(options) =>
+            options.filter((option) => option !== "")
+          }
+          loading={loading}
           value={orgName}
           onChange={(e, v) => setOrgName(v)}
-          getOptionLabel={(option) => option}
-          style={{ width: '100%' }}
-          renderInput={(params) => <TextField {...params} variant="outlined" />}
+          renderInput={(params) =>(
+            <TextField {...params}
+              variant="outlined"
+              InputProps={{
+                ...params.InputProps,
+                endAdornment: (
+                  <React.Fragment>
+                    {loading ? <CircularProgress color="inherit" size={20} /> : null}
+                    {params.InputProps.endAdornment}
+                  </React.Fragment>
+                ),
+              }}
+            />
+          )}
         />
       </Grid>
       <Grid item>
