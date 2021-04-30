1a997910e8f43aa5bf2bd45e67f4bf1bdb37b1b6
Bhaggya <bhaggya@gmail.com>
2021-04-29 17:37:41 -0700

    Changed How to do it page

--- src/components/TagGeneratorInstructions.js
+++ src/components/TagGeneratorInstructions.js
@@ -17,18 +17,23 @@ const useStyles = makeStyles(theme => ({
     backgroundColor: theme.palette.background.secondary,
   },
   btnText: {
-    backgroundColor: '#0CB2E7',
-    borderRadius: '12px',
+    borderRadius: '142.5px',
+    fontSize: '14px',
+    fontFamily: 'Work Sans',
+    paddingTop:'10px',
   },
   btnStyle: {
     width: '468px',
     height: '56px',
     paddingTop: '13px',
-    paddingLeft: '25rem',
+    paddingLeft: '38rem',
   },
   button: {
-    borderRadius: '5em',
+    width:'200px',
+    height: '36px',
+    borderRadius: '22.5px',
     backgroundColor: '#fff',
+    border: '1px solid #BCBCBC',
     color: '#6D6E74',
     fontSize: '14px',
     fontFamily: 'Work Sans',
@@ -36,30 +41,37 @@ const useStyles = makeStyles(theme => ({
       backgroundColor: '#fff',
     },
   },
+  btnClass: {
+    paddingLeft:'30px',
+    paddingTop:'30px',
+  },
   projectText: {
     fontSize: '32px',
+    fontWeight:'500',
     lineHeight: '40px',
-    fontWeight: 'bold',
     textAlign: 'center',
     paddingTop: '40px',
     color: '#042D5F',
+    fontFamily: 'Work Sans',
   },
   projText: {
     marginTop: '40px',
-    paddingLeft: '21rem',
+    paddingLeft: '30rem',
     paddingBottom: '64px',
   },
 
   boxItem: {
     border: '1px solid #BCBCBC',
-    borderRadius: '15px',
-    fontSize: '18px',
+    borderRadius: '12px',
     width: '272px',
     height: '152px',
   },
   boxText: {
     paddingTop: '30px',
-    paddingLeft: '37px',
+    paddingLeft: '46px',
+    color:'#0F1D2F',
+    fontSize: '18px',
+    fontFamily: 'Work Sans',
   },
 
 }));
@@ -69,7 +81,7 @@ const HowToUse = () => {
 
   return (
     <>
-      <Typography className={classes.projectsList}>
+      <Typography className={classes.projectsList} component="div">
         <List>
           <ListItem alignItems="flex-start" >
             <ListItemText primary={
@@ -92,7 +104,7 @@ const HowToUse = () => {
             <ListItemText primary={
               <Typography style={{ fontSize: '17px' }}>4. Repeat until you have finished adding all of your tags, then click Save Changes.</Typography>}>
             </ListItemText>
-            <ListItemText><img src="/images/step_4.png" alt="Step 4" /></ListItemText>
+            <ListItemText><img src="/images/step_4.png" alt="Step 4" style={{ paddingRight: '97px' }} /></ListItemText>
           </ListItem>
         </List>
       </Typography>
@@ -100,16 +112,16 @@ const HowToUse = () => {
         <Grid className={classes.projectText}> This project is so new we are celebrating every win</Grid>
       </Grid>
       <Box className={classes.btnStyle}>
-        <Button className={classes.btnText}>Let us know when you&apos;ve added  #civictechindex</Button>
+        <Button className={classes.btnText}>Let us know when you&apos;ve added <br/> #civictechindex</Button>
       </Box>
       <Grid container direction="row" className={classes.projText}>
         <Box component="div" className={classes.boxItem} p={1} m={1}>
           <Typography className={classes.boxText}>Add Another Project</Typography>
-          <Grid className={classes.boxText}> <Button variant="outlined" className={classes.button} href="/tag-generator">Tag Generator</Button></Grid>
+          <Grid className={classes.btnClass}> <Button className={classes.button} href="/tag-generator">Tag Generator</Button></Grid>
         </Box>
-        <Box component="div" container direction="row" className={classes.boxItem} p={1} m={1}>
+        <Box component="div" className={classes.boxItem} p={1} m={1}>
           <Typography className={classes.boxText}>Collaborate with us</Typography>
-          <Grid className={classes.boxText}> <Button variant="outlined" className={classes.button} href="/radicalcollaboration/faq">Learn More</Button></Grid>
+          <Grid className={classes.btnClass}> <Button className={classes.button} href="/radicalcollaboration/faq">Learn More</Button></Grid>
         </Box>
       </Grid>
     </>
