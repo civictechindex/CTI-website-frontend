857074e36554dd5c2be35eb6c90c6e60d52d75af
e0marina <emilymarinaro@gmail.com>
2021-05-01 14:47:35 -0700

    fixed conflict

--- src/components/index.js
+++ src/components/index.js
@@ -5,26 +5,6 @@ import NavBreadcrumbs from './NavBreadcrumbs'
 import NavButton from './NavButton'
 import PictureCard from './PictureCard'
 import TitleSection from './TitleSection'
-<<<<<<< HEAD
-import PhotoCardMobile from './PhotoCardMobile'
-import PhotoCardDsktp from './PhotoCardDsktp'
-import ProjectCard from '../pages/SearchProjects/ProjectCard'
-import SearchBar from '../pages/SearchProjects/SearchBar'
-
-export {
-  Footer,
-  GetStartedCard,
-  Header,
-  NavBreadcrumbs,
-  NavButton,
-  PhotoCardMobile,
-  PhotoCardDsktp,
-  PictureCard,
-  ProjectCard,
-  SearchBar,
-  TitleSection,
-}
-=======
 import ProjectCard from './ProjectCard'
 import SearchBar from '../pages/SearchProjects/SearchBar'
 
@@ -42,4 +22,3 @@ export {
   TitleSection,
 }
 
->>>>>>> fccd3c5a87bf9361ff8324b240f57121d6deca2f
