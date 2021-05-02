40b292a5d242c44c7da89303e8d318da5998e318
Bhaggya <bhaggya@gmail.com>
2021-05-01 18:32:47 -0700

    Subtle Things Fixed

--- src/components/TagGeneratorInstructions.js
+++ src/components/TagGeneratorInstructions.js
@@ -14,13 +14,14 @@ const useStyles = makeStyles(theme => ({
   btnText: {
     borderRadius: '142.5px',
     fontSize: '14px',
-    paddingTop:'10px',
+    paddingTop:'25px',
+    paddingBottom: '25px',
   },
   btnStyle: {
     width: '468px',
     height: '56px',
     paddingTop: '13px',
-    paddingLeft: '57px',
+    paddingLeft: '101px',
   },
   button: {
     width:'200px',
@@ -45,11 +46,13 @@ const useStyles = makeStyles(theme => ({
     lineHeight: '40px',
     paddingTop: '40px',
     color: '#042D5F',
+    textAlign: 'center',
+    paddingBottom: '20px',
   },
   projText: {
     marginTop: '40px',
     paddingBottom: '64px',
-    paddingRight: '82px',
+    paddingLeft: '40px',
   },
 
   boxItem: {
@@ -72,53 +75,53 @@ const HowToUse = () => {
   return (
     <>
       <Box className='containerGray'>
-      <Container>
-        <List>
-          <ListItem alignItems="flex-start" >
-            <ListItemText primary={
-              <Typography style={{ fontSize: '17px', paddingBottom: '12px' }}> 1. Navigate to your project&apos;s repository in another browser to add your generated tags.</Typography>}>
-            </ListItemText>
-          </ListItem>
-          <ListItem alignItems="flex-start">
-            <ListItemText primary={
-              <Typography style={{ fontSize: '17px' }}>2. Under your project’s repository, click to paste your tags.</Typography>}>
-            </ListItemText>
-            <ListItemText><img src="/images/step_2.png" alt="Step 2" /></ListItemText>
-          </ListItem>
-          <ListItem alignItems="flex-start">
-            <ListItemText primary={
-              <Typography style={{ fontSize: '17px' }}>3. Under &quot;Topics&quot;, paste the topic you want to add to your repository.</Typography>}>
-            </ListItemText>
-            <ListItemText><img src="/images/step_3.png" alt="Step 3" /></ListItemText>
-          </ListItem>
-          <ListItem alignItems="flex-start">
-            <ListItemText primary={
-              <Typography style={{ fontSize: '17px' }}>4. Repeat until you have finished adding all of your tags, then click Save Changes.</Typography>}>
-            </ListItemText>
-            <ListItemText><img src="/images/step_4.png" alt="Step 4" style={{ paddingRight: '97px' }} /></ListItemText>
-          </ListItem>
-        </List>
-      </Container>
+        <Container>
+          <List>
+            <ListItem alignItems="flex-start" >
+              <ListItemText primary={
+                <Typography style={{ fontSize: '17px', paddingBottom: '12px' }}> 1. Navigate to your project&apos;s repository in another browser to add your generated tags.</Typography>}>
+              </ListItemText>
+            </ListItem>
+            <ListItem alignItems="flex-start">
+              <ListItemText primary={
+                <Typography style={{ fontSize: '17px' }}>2. Under your project’s repository, click to paste your tags.</Typography>}>
+              </ListItemText>
+              <ListItemText><img src="/images/step_2.png" alt="Step 2" /></ListItemText>
+            </ListItem>
+            <ListItem alignItems="flex-start">
+              <ListItemText primary={
+                <Typography style={{ fontSize: '17px' }}>3. Under &quot;Topics&quot;, paste the topic you want to add to your repository.</Typography>}>
+              </ListItemText>
+              <ListItemText><img src="/images/step_3.png" alt="Step 3" /></ListItemText>
+            </ListItem>
+            <ListItem alignItems="flex-start">
+              <ListItemText primary={
+                <Typography style={{ fontSize: '17px' }}>4. Repeat until you have finished adding all of your tags, then click Save Changes.</Typography>}>
+              </ListItemText>
+              <ListItemText><img src="/images/step_4.png" alt="Step 4" style={{ paddingRight: '97px' }} /></ListItemText>
+            </ListItem>
+          </List>
+        </Container>
       </Box>
       <Box className='containerWhite'>
-      <Container>
-      <Grid container alignItems="center" justify="center" direction="column">
-        <Grid className={classes.projectText}> This project is so new we are celebrating every win</Grid>
-        <Grid className={classes.btnStyle}>
-          <Button className={classes.btnText}>Let us know when you&apos;ve added <br/> #civictechindex</Button>
-        </Grid>
-        <Grid container direction="row" className={classes.projText} alignItems="center" justify="center">
-          <Box component="div" className={classes.boxItem} p={1} m={1}>
-            <Typography className={classes.boxText} color='primary'>Add Another Project</Typography>
-            <Grid className={classes.btnClass}> <Button className={classes.button} href="/tag-generator">Tag Generator</Button></Grid>
-          </Box>
-          <Box component="div" className={classes.boxItem} p={1} m={1}>
-            <Typography className={classes.boxText} color='primary'>Collaborate with us</Typography>
-            <Grid className={classes.btnClass}> <Button className={classes.button} href="/radicalcollaboration/faq">Learn More</Button></Grid>
-          </Box>
-        </Grid>
-      </Grid>
-      </Container>
+        <Container>
+          <Grid container alignItems="center" justify="center" direction="column">
+            <Grid className={classes.projectText}> This project is so new, <br/>we are celebrating every win!</Grid>
+            <Grid className={classes.btnStyle}>
+              <Button className={classes.btnText}>Let us know when you&apos;ve added <br/> #civictechindex</Button>
+            </Grid>
+            <Grid container direction="row" className={classes.projText} alignItems="center" justify="center">
+              <Box component="div" className={classes.boxItem} p={1} m={1}>
+                <Typography className={classes.boxText} color='primary'>Add Another Project</Typography>
+                <Grid className={classes.btnClass}> <Button className={classes.button} href="/tag-generator">Tag Generator</Button></Grid>
+              </Box>
+              <Box component="div" className={classes.boxItem} p={1} m={1}>
+                <Typography className={classes.boxText} color='primary'>Collaborate with us</Typography>
+                <Grid className={classes.btnClass}> <Button className={classes.button} href="/support">Learn More</Button></Grid>
+              </Box>
+            </Grid>
+          </Grid>
+        </Container>
       </Box>
     </>
   )
