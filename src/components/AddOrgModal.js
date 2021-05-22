e35b78ad4bb5c8dfab1c491ba6bc0202900595b4
Maxwell Countryman Skewes <maxskewes@gmail.com>
2021-05-22 00:50:30 -0700

    Update AddOrgModal.js
    Centered form, centered modal, added effect, added click anywhere close

--- src/components/AddOrgModal.js
+++ src/components/AddOrgModal.js
@@ -9,8 +9,14 @@ import Typography from '@material-ui/core/Typography';
 import InputAdornment from '@material-ui/core/InputAdornment';
 import HelpIcon from '@material-ui/icons/Help';
 import makeStyles from '@material-ui/core/styles/makeStyles';
+import Fade from '@material-ui/core/Fade';
 
 const useStyles = makeStyles((theme) => ({
+  modal: {
+    display: 'flex',
+    flexWrap: 'wrap',
+    justifyContent: 'center',
+  },
   AddOrgStyle: {
     backgroundColor: theme.palette.background.primary,
     backgroundImage: 'url(/images/world-view.png)',
@@ -110,108 +116,114 @@ const AddOrgModal = ({ open, onClose }) => {
     <>
       <Modal
         open={open}
+        onClose={handleClose}
+        aria-labelledby="transition-modal-title"
+        className={classes.modal}
         style={{ overflow:'scroll' }}
       >
-        <Grid container style={{ justifyContent: 'center' }} className={classes.AddOrgStyle}>
-          <Grid item xs={1} />
-          <Grid 
-            item xs={10}
-            container
-            direction='column'
-          >
-
-            <Typography variant='h1' style={{ textAlign: 'center' }}>Add Organization to the Civic Tech Index</Typography>
-            
-            <Grid item xs={12}>
-              <Typography variant='h2'>Organization Detail</Typography>
-              <Typography variant='h3'>Organization Name:*</Typography>
-              <TextField id='organization-name' name='name' onChange={handleInputChange}></TextField>
-              <Typography variant='h3'>Parent Organization:</Typography>
-              <TextField id='parent-organization' name='parent_organization' onChange={handleInputChange}></TextField>
-            </Grid>
-
-            <Grid item xs={12}>
-              <Typography variant='h2'>Project URL</Typography>
-              <Typography variant='h3'>Website URL:*</Typography>
-              <TextField id='project-website-url' name='website_url' onChange={handleInputChange}></TextField>
-              <Typography variant='h3'>Github URL:*</Typography>
-              <TextField id='project-github-url' name='github_url' onChange={handleInputChange}></TextField>
-            </Grid>
-
-            <Grid item xs={12}>
-              <Typography variant='h2'>Social Media URL</Typography>
-              <Typography variant='h3'>Facebook URL:</Typography>
-              <TextField id='facebook-url' name='facebook_url' onChange={handleInputChange}></TextField>
-              <Typography variant='h3'>Twitter URL:</Typography>
-              <TextField id='twitter-url' name='twitter_url' onChange={handleInputChange}></TextField>
-              <Typography variant='h3'>MeetUp URL:</Typography>
-              <TextField id='meetup-url' name='meetup_url' onChange={handleInputChange}></TextField>
-            </Grid>
-            
-            <Grid item xs={12}>
-              <Typography variant='h2'>Location</Typography>
-            </Grid>
-            <Grid container item xs={12} spacing={2}>
-              <Grid item xs={12} sm={4}>
-                <Typography variant='h3'>City</Typography>
-                <TextField id='location-city' name='city' onChange={handleInputChange}></TextField>
-              </Grid>
-              <Grid item xs={12} sm={3}>
-                <Typography variant='h3'>State/Prov./Co.</Typography>
-                <TextField id='location-state-prov-co' name='state' onChange={handleInputChange}></TextField>
-              </Grid>
-              <Grid item xs={12} sm={5}>
-                <Typography variant='h3'>Country:</Typography>
-                <CountrySelect style={{ width: '100%' }} onChange={handleCountryChange}/>
-              </Grid>
-            </Grid>
-
-            <Grid item xs={12}>
-              <Typography variant='h2'>Github Tags</Typography>
-              <TextField
-              id='github-tags'
-              name='org_tag'
-              InputProps={{
-                endAdornment: (
-                  <InputAdornment position='start'>
-                    <HelpIcon style={{ blend: 'pass-through' }}/>
-                  </InputAdornment>
-                ),
-              }}
-              onChange={handleInputChange}
+        <Fade in={open}>
+          <Grid container style={{ justifyContent: 'center' }} className={classes.AddOrgStyle}>
+            <Grid item xs={1} />
+            <Grid 
+              item xs={10}
+              container
+              direction='column'
             >
-            </TextField>
-            </Grid>
-            <Grid item xs={12}>
-              <Typography variant='h2'>Organization Email*</Typography>
-              <TextField id='organization-email' name='organization_email' onChange={handleInputChange}></TextField>
-            </Grid>
 
-            {/* //Buttons */}
-            <Grid container justify='center' alignItems='center' style={{ height: '100px', padding: '40px 0px' }}>
-              <Grid style={{ textAlign: 'center' }}>
-                <Button
-                  style={{ width: '270px', padding: '10px', margin: '12px' }}
-                  variant='contained'
-                  color='default'
-                  onClick={() => handleClose(null)}
-                >
-                  Cancel
-                </Button>
+              <Typography variant='h1' style={{ textAlign: 'center' }}>Add Organization to the Civic Tech Index</Typography>
+              
+              <Grid item xs={12}>
+                <Typography variant='h2'>Organization Detail</Typography>
+                <Typography variant='h3'>Organization Name:*</Typography>
+                <TextField id='organization-name' name='name' onChange={handleInputChange}></TextField>
+                <Typography variant='h3'>Parent Organization:</Typography>
+                <TextField id='parent-organization' name='parent_organization' onChange={handleInputChange}></TextField>
               </Grid>
-              <Grid style={{ textAlign: 'center' }}>
-                <Button
-                  style={{ width: '270px', padding: '10px', margin: '12px' }}
-                  variant='contained'
-                  color='secondary'
-                  onClick={() => handleSave()}
-                >
-                  Save
-                </Button>
-              </Grid>    
+
+              <Grid item xs={12}>
+                <Typography variant='h2'>Project URL</Typography>
+                <Typography variant='h3'>Website URL:*</Typography>
+                <TextField id='project-website-url' name='website_url' onChange={handleInputChange}></TextField>
+                <Typography variant='h3'>Github URL:*</Typography>
+                <TextField id='project-github-url' name='github_url' onChange={handleInputChange}></TextField>
+              </Grid>
+
+              <Grid item xs={12}>
+                <Typography variant='h2'>Social Media URL</Typography>
+                <Typography variant='h3'>Facebook URL:</Typography>
+                <TextField id='facebook-url' name='facebook_url' onChange={handleInputChange}></TextField>
+                <Typography variant='h3'>Twitter URL:</Typography>
+                <TextField id='twitter-url' name='twitter_url' onChange={handleInputChange}></TextField>
+                <Typography variant='h3'>MeetUp URL:</Typography>
+                <TextField id='meetup-url' name='meetup_url' onChange={handleInputChange}></TextField>
+              </Grid>
+              
+              <Grid item xs={12}>
+                <Typography variant='h2'>Location</Typography>
+              </Grid>
+              <Grid container item xs={12} spacing={2}>
+                <Grid item xs={12} sm={4}>
+                  <Typography variant='h3'>City</Typography>
+                  <TextField id='location-city' name='city' onChange={handleInputChange}></TextField>
+                </Grid>
+                <Grid item xs={12} sm={3}>
+                  <Typography variant='h3'>State/Prov./Co.</Typography>
+                  <TextField id='location-state-prov-co' name='state' onChange={handleInputChange}></TextField>
+                </Grid>
+                <Grid item xs={12} sm={5}>
+                  <Typography variant='h3'>Country:</Typography>
+                  <CountrySelect style={{ width: '100%' }} onChange={handleCountryChange}/>
+                </Grid>
+              </Grid>
+
+              <Grid item xs={12}>
+                <Typography variant='h2'>Github Tags</Typography>
+                <TextField
+                id='github-tags'
+                name='org_tag'
+                InputProps={{
+                  endAdornment: (
+                    <InputAdornment position='start'>
+                      <HelpIcon style={{ blend: 'pass-through' }}/>
+                    </InputAdornment>
+                  ),
+                }}
+                onChange={handleInputChange}
+              >
+              </TextField>
+              </Grid>
+              <Grid item xs={12}>
+                <Typography variant='h2'>Organization Email*</Typography>
+                <TextField id='organization-email' name='organization_email' onChange={handleInputChange}></TextField>
+              </Grid>
+
+              {/* //Buttons */}
+              <Grid container justify='center' alignItems='center' style={{ height: '100px', padding: '40px 0px' }}>
+                <Grid style={{ textAlign: 'center' }}>
+                  <Button
+                    style={{ width: '270px', padding: '10px', margin: '12px' }}
+                    variant='contained'
+                    color='default'
+                    onClick={() => handleClose(null)}
+                  >
+                    Cancel
+                  </Button>
+                </Grid>
+                <Grid style={{ textAlign: 'center' }}>
+                  <Button
+                    style={{ width: '270px', padding: '10px', margin: '12px' }}
+                    variant='contained'
+                    color='secondary'
+                    onClick={() => handleSave()}
+                  >
+                    Save
+                  </Button>
+                </Grid>    
+              </Grid>
             </Grid>
+            <Grid item xs={1} />
           </Grid>
-        </Grid>
+        </Fade>
       </Modal>
     </>
   );
