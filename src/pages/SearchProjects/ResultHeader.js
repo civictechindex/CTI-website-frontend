8ce1bace86d191b007e088f97d0fd7e4568dc4f0
Travis Liang <travisliang001@gmail.com>
2021-06-13 15:26:02 -0600

    Search projects 626  (#643)
    * Fixes for Search Projects (#626)
    
    * Make search bar icon functional
    
    * Update "Clear all" hover style
    
    * Resolved the issues 'Sort by Last Updated not working properly'
    
    * Fixes for Search Projects (#626)
    
    * Make search bar icon functional
    
    * Update "Clear all" hover style
    
    * Fixes for Search Projects (#626)
    
    * Add message for Github 403 errors
    
    * Revert the changes to resolve the last_updated sort issues.
    
    * Remove (comment out) the 'last_updated' option in the dropdown on the search page
    
    * Comment out "Last Updated" in the left-hand Filter, as well
    
    * Comment out "Last Updated" in the left-hand Filter, as well
    
    Co-authored-by: Bruce Lai <bruceplai@yahoo.com>
    Co-authored-by: Kevin Ashworth <kevinashworth@yahoo.com>

--- src/pages/SearchProjects/ResultHeader.js
+++ src/pages/SearchProjects/ResultHeader.js
@@ -50,7 +50,10 @@ const ResultHeader = (props) => {
             onChange={(e) => props.onSortChange(e.target.value)}
           >
             <MenuItem value='best match'>Best Match</MenuItem>
-            <MenuItem value='updated'>Last Updated</MenuItem>
+            {/* 
+              Issue#626: This is the temporary fix that removes the 'Last Updated' option from Dropdown in the search page.
+              <MenuItem value='updated'>Last Updated</MenuItem> 
+            */}
             <MenuItem value='stars'>Stargazer Count</MenuItem>
           </Select>
         </FormControl>
