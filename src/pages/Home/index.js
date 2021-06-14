a4ef625c2297a35fe0457ba999989d0d2dbae1a6
e0marina <emilymarinaro@gmail.com>
2021-06-14 09:55:50 -0700

    working on alignment of yellow title and marketing section

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,8 +1,11 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 import makeStyles from '@material-ui/core/styles/makeStyles';
 import Box from '@material-ui/core/Box';
 import Container from '@material-ui/core/Container';
 import Grid from '@material-ui/core/Grid';
+import Hidden from '@material-ui/core/Hidden';
+import withWidth from '@material-ui/core/withWidth';
 import { GetStartedCard, NavButton, TitleSection } from '../../components';
 import NotableUsersSection from './sections/NotableUsersSection';
 import TrendingTopicsSection from './sections/TrendingTopicsSection';
@@ -14,8 +17,9 @@ const useStyles = makeStyles((theme) => ({
     fontSize: '18px',
     paddingTop: '8px',
     '& p': {
-      width: '33.3%',
+      width: '29.3%',
     },
+    justifyContent: 'center',
     [theme.breakpoints.down('md')]: {
       fontSize: '16px',
     },
@@ -34,10 +38,8 @@ const useStyles = makeStyles((theme) => ({
     },
   },
   localTitleSectionStyle: {
-    paddingTop: '120px',
-    [theme.breakpoints.down('md')]: {
-      paddingTop: '72px',
-    },
+    paddingTop: '5%',
+    width: '113%',
   },
 }));
 
@@ -47,19 +49,20 @@ const MarketingSection = () => {
     <Grid
       container
       align='center'
-      className={classes.marketingPointContainerStyle}>
+      className={classes.marketingPointContainerStyle}
+    >
       <Typography variant='body1' color='textSecondary'>
-                Requires no coding
+        Requires no coding
       </Typography>
       <Typography variant='body1' color='textSecondary'>
         {' '}
-                Make your project more <br /> visible with GitHub’s open-source
-                communities
+        Make your project more <br /> visible with GitHub’s open-source
+        communities
       </Typography>
       <Typography variant='body1' color='textSecondary'>
         {' '}
-                The Index is owned by <br />
-                all who use it
+        The Index is owned by <br />
+        all who use it
       </Typography>
     </Grid>
   );
@@ -70,10 +73,10 @@ const CallToActionSection = () => {
     <Grid container style={{ paddingTop: '35px' }} justify='center'>
       <Grid container justify='space-between' style={{ width: '392px' }}>
         <NavButton href='/tag-generator' color='primary'>
-                    Tag your project
+          Tag your project
         </NavButton>
         <NavButton href='/about' variant='outlined'>
-                    Learn more
+          Learn more
         </NavButton>
       </Grid>
     </Grid>
@@ -86,13 +89,13 @@ const Home = () => {
     <Box className='boxBackground'>
       <div className='containerWorld'>
         <Container>
-          <Box
-            component='div'
-            className={classes.localTitleSectionStyle}>
-            <TitleSection>
-                            Join a worldwide movement to catalog every open
-                            source civic tech project.
-            </TitleSection>
+          <Box component='div' className={classes.localTitleSectionStyle}>
+            <Hidden mdDown>
+              <TitleSection>
+              Join a worldwide movement to catalog every open source civic tech
+              project.
+              </TitleSection>
+            </Hidden>
           </Box>
           <MarketingSection />
           <CallToActionSection />
@@ -108,5 +111,8 @@ const Home = () => {
     </Box>
   );
 };
+Home.propTypes = {
+  width: PropTypes.oneOf(['lg', 'md', 'sm', 'xl', 'xs']).isRequired,
+};
 
-export default Home;
+export default withWidth()(Home);
