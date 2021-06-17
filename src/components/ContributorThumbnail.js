c9aac7c1f11ae1905cc00e12204433fd5392d516
Bhaggya <bhaggya@gmail.com>
2021-06-17 16:41:20 -0700

    Redesign Organization Page as perFigma

--- src/components/ContributorThumbnail.js
+++ src/components/ContributorThumbnail.js
@@ -1,56 +1,74 @@
 import React, { useEffect, useState } from "react";
 import { getOrganizationLinks } from "./getOrganizationLinks.js";
 import { useStyle } from "../pages/Contributors/styles";
-import Typography from '@material-ui/core/Typography';
 import Link from '@material-ui/core/Link';
 import CardMedia from '@material-ui/core/CardMedia';
 import Container from "@material-ui/core/Container";
 import Grid from '@material-ui/core/Grid';
+import { Typography } from "@material-ui/core";
+import Box from '@material-ui/core/Box';
 
-export const ContributorThumbnail = ({ organization }) => {
+export const ContributorThumbnail = ({ organization, isOpen }) => {
   const classes = useStyle();
 
+
   const [thumbnailInfo, setThumbnailInfo] = useState({});
+
   useEffect(() => {
+
+
     setThumbnailInfo(getOrganizationLinks(organization));
+
   }, [organization]);
 
+
+
   return (
-    <Container className={classes.thumbnailWrapper}>
-      {thumbnailInfo.organizationUrl ? (
-        <Link
-          href={thumbnailInfo.organizationUrl}
-          className={classes.thumbnails}
-          target="_blank"
-          rel="noreferrer noopener"
-        >
-          {thumbnailInfo.imageUrl ? (
-            <Thumbnail
-              thumbnailInfo={thumbnailInfo}
-              organization={organization}
-            />
-          ) : (
-            <Grid className={classes.textWrapperWithoutImage}>
-              <Typography className={classes.thumbnailTextWithoutImage} component="div">
-                {organization.name ? organization.name : organization}
-              </Typography>
-            </Grid>
-          )}
-        </Link>
-      ) : (
-        <Grid className={classes.textWrapperWithoutImage}>
-            No URL Data for {organization.name}
-        </Grid>
-      )}
-    </Container>
+
+
+    <>
+      <Container className={classes.thumbnailWrapper} component="span">
+        {thumbnailInfo.organizationUrl ? (
+
+          <Link
+            href={thumbnailInfo.organizationUrl}
+            className={classes.thumbnails}
+            target="_blank"
+            rel="noreferrer noopener"
+          >
+            {thumbnailInfo.imageUrl ? (
+
+              <Thumbnail
+                thumbnailInfo={thumbnailInfo}
+                organization={organization}
+                isOpen={isOpen}
+              />
+            ) : (
+              <Grid className={classes.textWrapperWithoutImage} component="span">
+                <Grid className={classes.thumbnailTextWithoutImage} component="span">
+                  <Typography component="span" className={isOpen ? `${classes.blueColorText}` : `${classes.orgText}`}>{organization.name ? organization.name : organization} </Typography>
+                </Grid>
+              </Grid>
+            )}
+          </Link>
+        ) : (
+          <Grid className={classes.textWrapperWithoutImage} component="span">
+            <Typography component="span"> No URL Data for {organization.name} </Typography>
+          </Grid>
+        )}
+      </Container>
+    </>
   );
 };
 
-const Thumbnail = ({ thumbnailInfo, organization }) => {
+
+
+const Thumbnail = ({ thumbnailInfo, organization, isOpen }) => {
   const classes = useStyle();
+
   return (
     <>
-      <Grid className={classes.imageWrapper}>
+      <Grid className={classes.imageWrapper} component="span">
         <CardMedia
           component="img"
           src={thumbnailInfo.imageUrl}
@@ -64,11 +82,15 @@ const Thumbnail = ({ thumbnailInfo, organization }) => {
           loading="lazy"
         />
       </Grid>
-      <Grid className={classes.textWrapper}>
-        <Grid className={classes.thumbnailText}>
-          {organization.name ? organization.name : organization}
-        </Grid>
+
+      <Grid data-cy="affthumbnailTextWrapper" className={classes.textWrapper} component="span">
+        <Box data-cy="affthumbnailText" className={classes.thumbnailText} component="span">
+          <Typography  component={'span'} data-cy='thumbnailTextInfn' className={isOpen ? `${classes.blueColorText}` : `${classes.orgText}`}> {organization.name ? organization.name : organization} </Typography>
+        </Box>
+
       </Grid>
+
+
     </>
   );
 };
--- src/pages/Contributors/index.js
+++ src/pages/Contributors/index.js
@@ -12,239 +12,501 @@ import Typography from "@material-ui/core/Typography";
 import Autocomplete from "@material-ui/lab/Autocomplete";
 import SearchRoundedIcon from '@material-ui/icons/SearchRounded';
 import NavBreadcrumbs from "../../components/NavBreadcrumbs";
-import { DropdownArrow } from "../../components/DropdownArrow.js";
-import { AffiliatedOrganizations } from "./AffiliatedOrganizations";
-import { UnaffiliatedOrganizations } from "./UnaffiliatedOrganizations";
 import { useStyle } from "./styles.js";
 import GetStartedCard from '../../components/GetStartedCard'
+import { TitleSection } from '../../components'
+import Grid from '@material-ui/core/Grid';
+import { makeStyles } from "@material-ui/core";
+import PropTypes from "prop-types";
+import AppBar from "@material-ui/core/AppBar";
+import Tabs from "@material-ui/core/Tabs";
+import Tab from "@material-ui/core/Tab";
+import Checkbox from '@material-ui/core/Checkbox';
+import FormGroup from "@material-ui/core/FormGroup";
+import FormControlLabel from "@material-ui/core/FormControlLabel";
+import { MuiThemeProvider, createMuiTheme } from "@material-ui/core/styles";
+import { Affiliated  } from "./Affiliated";
+import { UnaffiliatedOrganizations } from "./UnaffiliatedOrganizations";
+
+// eslint-disable-next-line
+function TabPanel(props) {
+  const { children, value, index, ...other } = props;
+
+  return (
+    <Grid
+      role="tabpanel"
+      hidden={value !== index}
+      id={`full-width-tabpanel-${index}`}
+      aria-labelledby={`full-width-tab-${index}`}
+      {...other}
+    >
+      {value === index && (
+        <Box p={3}>
+          <Box>{children}</Box>
+        </Box>
+      )}
+    </Grid>
+  );
+}
+
 
 
 export default function Contributors({ match }) {
+
   const affiliation = match.params.affiliation;
+
+  const searchaffiliation = match.params.searchaffiliation;
+
 
   const classes = useStyle();
 
   const [organizations, setOrganizations] = useState([]);
   const [organizationNamesList, setOrganizationNamesList] = useState([]);
-  const [
-    affiliatedOrganizationsObject,
-    setAffiliatedOrganizationsObject,
-  ] = useState({});
+
+  const [affiliatedOrganizationsObject, setAffiliatedOrganizationsObject] = useState({});
+
   const [affiliatedOpen, setAffiliatedOpen] = useState(false);
+
+  const [affiliatedSepOpen, setAfflnSepOpen] = useState(false);
+
+
   const [unaffiliatedOpen, setUnaffiliatedOpen] = useState(false);
   const [inputValue, setInputValue] = useState("");
 
+  const [unaffiliated, getAffiliatedNames] = useState([]);
+  const [unaffiliatedCount, getunaffiliatedCount] = useState(0);
+  const [affiliatedCount, getaffiliatedCount] = useState(0);
+  const [organizationData, getOrganizationData] = useState([]);
+
+  const [searchCount, setsearchCount] = useState(false);
+
+
+  let count1 =0, count2 =0,totalaffiliatedCount=0,totalunaffiliatedCount=0;
+
   useEffect(() => {
     const fetchData = async () => {
-      const result = await axios.get(
-        `${process.env.REACT_APP_API_URL}/api/organizations/`
-      );
-      const sorted = result.data.sort((a, b) => a.id - b.id);
+
+
+      const result = await axios.get(`${process.env.REACT_APP_API_URL}/api/organizations/`)
+
+      const organization = result.data;
+
+
+      const sorted = organization.sort((a, b) => a.id - b.id);
       setOrganizations(sorted);
+
     };
 
     fetchData();
+
   }, []);
 
+
   useEffect(() => {
-    const createAffiliatedOrganizations = () => {
+    const createAffiliatedOrganizations = () =>
+    {
       const input = inputValue.toLowerCase().replace(/\s/g, "");
       const affiliated = Object.create(null);
       // iterate through the json response
       const names = [];
+
       const addToAffiliated = (organization) => {
+
         if (!affiliated["Code for All"]) {
           affiliated["Code for All"] = [];
         }
-        // check if the parent of the affiliated org is in the object
-        if (!affiliated[organization.parent_organization.name]) {
-          // if YES the parent is a key in the object, then add this current org to it's value array
-          affiliated["Code for All"].push(
-            organizations[organization.parent_organization.id - 1]
-          );
-          // if NO, add the parent as a key, AND add this current org to the value array
-          affiliated[organization.parent_organization.name] = [organization];
-        } else {
-          affiliated[organization.parent_organization.name].push(organization);
-          if (organization.parent_organization.name === "Code for All") {
-            affiliated[organization.name] = [];
-          }
+        if (affiliated["Code for All"])
+        {
+
+          affiliated["Code for All"].push(organizations[organization.id - 2]);
+          affiliated[organization.name] = [organization];
+
         }
+
+
+
       };
+
       const addToUnaffiliated = (organization) => {
+
+
         if (affiliated["unaffiliated"]) {
-          // if YES the parent is a key in the object, then add this current org to it's value array
           affiliated["unaffiliated"].push(organization);
-        } else {
-          // if NO, add the parent as a key, AND add this current org to the value array
+        }
+        else {
           affiliated["unaffiliated"] = [organization];
         }
+
+        getAffiliatedNames(organization);
+
       };
-      for (const org of organizations) {
+
+
+      for (const org of organizations)
+      {
         names.push(org.name);
         const orgName = org.name.toLowerCase().replace(/\s/g, "");
-        if ((!inputValue || orgName.includes(input)) && org.id !== 2) {
-          // find orgs that are affiliated
-          if (org.parent_organization) {
-            addToAffiliated(org);
-          } else {
+        if (!inputValue || orgName.includes(input))
+        {
+          if (org.affiliated === false)
+          {
+            count1++;
             addToUnaffiliated(org);
           }
+          else if (org.affiliated === true)
+          {
+            count2++;
+            addToAffiliated(org);
+          }
         }
+        getOrganizationData(organizations);
+
+      }
+
+
+      if (count1 !== 0 || count2 !== 0)
+      {
+
+        if (inputValue !== '')
+        {
+          getunaffiliatedCount(count1);
+          getaffiliatedCount(count2);
+
+          setsearchCount(true);
+
+
+        }
+
       }
 
       setAffiliatedOrganizationsObject(affiliated);
       setOrganizationNamesList(names.sort());
+
     };
+
     createAffiliatedOrganizations();
-  }, [organizations, inputValue]);
+  }, [organizations, inputValue, count1,count2,searchaffiliation,organizationData,searchCount,unaffiliatedCount,affiliatedCount]);
+
+
+  if (organizationData.length >0)
+  {
+
+    for (const orgdata of organizationData)
+    {
+
+      if (orgdata.depth  === 3 || orgdata.depth === 4)
+      {
+        totalaffiliatedCount++;
+
+      }
+      if (orgdata.depth  === 2 && orgdata.name !== 'Code for All')
+      {
+        totalunaffiliatedCount++;
+      }
+    }
+
+
+  }
+
 
   useEffect(() => {
     if (affiliation === "unaffiliated") {
       setUnaffiliatedOpen(true);
       setAffiliatedOpen(false);
+      setAfflnSepOpen(false);
     } else if (affiliation === "affiliated") {
       setAffiliatedOpen(true);
       setUnaffiliatedOpen(false);
+      setAfflnSepOpen(true);
     } else {
-      setAffiliatedOpen(true);
+      setAffiliatedOpen(false);
       setUnaffiliatedOpen(true);
+      setAfflnSepOpen(false);
     }
   }, [affiliation]);
 
+
+
+  // Tab Code
+
+  TabPanel.propTypes = {
+    children: PropTypes.node,
+    index: PropTypes.any.isRequired,
+    value: PropTypes.any.isRequired,
+  };
+  // eslint-disable-next-line
+  function a11yProps(index) {
+    return {
+      id: `full-width-tab-${index}`,
+      "aria-controls": `full-width-tabpanel-${index}`,
+    };
+  }
+
+  const theme = createMuiTheme({
+
+    overrides: {
+      MuiTab: {
+        "root": {
+          color: 'theme​.palette.​text.disabled',
+          fontSize: '32px',
+          fontWeight: 'bold',
+          textTransform: 'none',
+          display: "flex",
+          '&$selected': {
+            color: '#006B95',
+          },
+
+        },
+        wrapper: {
+          flexDirection: "row",
+          width: "auto",
+        },
+
+      },
+
+    },
+  });
+
+  const [value, setValue] = React.useState(0);
+
+  const handleChange = (event, newValue) => {
+    setValue(newValue);
+
+  };
+
+
+  // CheckBox Code
+
+
+  const [checkboxValue, setIsTrue] = useState(false);
+
+
+  const checkBoxChange = (event) => {
+    const target = event.target.checked;
+    setIsTrue(target);
+  };
+
+
   return (
-    <Box className='containerDefault'>
-      <Container>
-        <div className={classes.firstSectionWrapper}>
-          <div className={classes.sectionContainer}>
-            <NavBreadcrumbs
-              crumbs={[
-                { name: "Home", href: "/home" },
-                { name: "Organizations", href: "/organizations/all" },
-              ]}
-            />
-          </div>
-          <div className={classes.sectionContainer}>
-            <TopCallToAction
-              heading="Index Contributors"
-              tagline="Check out our partners who have contributed to the Civic Tech Index"
-              options={organizationNamesList}
-              inputValue={inputValue}
-              setInputValue={setInputValue}
-              inputPlaceholder="Search for an organization"
-            />
-          </div>
-        </div>
-        <div className={classes.unaffiliatedWrapper}>
-          <div className={classes.sectionContainer}>
-            <div className={classes.affiliation}>
-              <Typography variant='h2' color='primary'>
-              Unaffiliated Contributors
-              </Typography>
-              <DropdownArrow setOpenFunction={setUnaffiliatedOpen} />
-            </div>
-            <div>
-              {unaffiliatedOpen && (
-                <Affiliation
-                  organizations={affiliatedOrganizationsObject["unaffiliated"]}
-                  inputValue={inputValue}
-                  classes={classes}
-                  affiliation="unaffiliated"
-                />
-              )}
-            </div>
-          </div>
-        </div>
-        <div className={classes.affiliatedWrapper}>
-          <div className={classes.sectionContainer}>
-            <div className={classes.affiliation}>
-              <Typography variant='h2' color='textPrimary'>
-              Affiliated Contributors
-              </Typography>
-              <DropdownArrow setOpenFunction={setAffiliatedOpen} />
-            </div>
-            <div className={classes.affiliatedOrgsContainer}>
-              {affiliatedOpen && (
-                <Affiliation
-                  organizations={affiliatedOrganizationsObject}
-                  inputValue={inputValue}
-                  classes={classes}
-                  affiliation="affiliated"
-                />
-              )}
-            </div>
-          </div>
-        </div>
-      </Container>
-      <GetStartedCard
-        headerTitle='Want to add your organization?'
-        buttonText='ContatUs'
-        buttonHref='/contactus'
-      />
+
+    <Box className='pageContainer'>
+      <Box className='containerDefault'>
+        <Container className={classes.firstSectionWrapper}>
+          <NavBreadcrumbs
+            crumbs={[
+              { name: "Home", href: "/" },
+              { name: "Organizations", href: "/contributors" },
+            ]}
+          />
+          <Grid container>
+            <TitleSection>Organizations</TitleSection>
+            <Grid item xs={12}>
+              <Typography color='textSecondary' className={classes.textStyle}>Check out our partners who have contributed to the Civic Tech Index</Typography>
+            </Grid>
+            <Grid item xs={12}>
+
+              <TopCallToAction
+                options={organizationNamesList}
+                inputValue={inputValue}
+                setInputValue={setInputValue}
+                inputPlaceholder="Search for an organization"
+              />
+            </Grid>
+          </Grid>
+        </Container>
+      </Box>
+      <Box className='containerGray'>
+
+        <Container>
+
+
+          <MuiThemeProvider theme={theme}>
+            <AppBar position="static" color="default" elevation={0}>
+              <Tabs
+                value={value}
+                onChange={handleChange}
+                variant="fullWidth"
+                className={classes.tabs}
+                classes={{ indicator: classes.indicator }}
+              >
+
+                <Tab label={<><span  style={{ display:'flex',alignItems:'center', paddingLeft: '10px' }}>({totalunaffiliatedCount + totalaffiliatedCount })</span></>} icon="All" {...a11yProps(0)} className={classes.tabVal} />
+                <Tab  icon="Unaffiliated"  label={<><span  style={{ display:'flex',alignItems:'center', paddingLeft: '10px' }}>({affiliatedOrganizationsObject["unaffiliated"] ? affiliatedOrganizationsObject["unaffiliated"].length : 0})</span></>} className={classes.tabVal} {...a11yProps(1)} />
+                <Tab  icon="Affiliated" label={<><span  style={{ display:'flex',alignItems:'center', paddingLeft: '10px' }}>({totalaffiliatedCount})</span></>} className={classes.tabVal} {...a11yProps(2)} />
+              </Tabs>
+            </AppBar>
+          </MuiThemeProvider>
+
+
+
+          <Grid index={value}>
+            <Grid>
+              <FormGroup>
+                <FormControlLabel
+                  control={
+                    <Checkbox className={classes.chkBoxStyle}
+                      onChange={checkBoxChange} defaultChecked={false}
+                    />
+                  }
+                  label={<Typography className={classes.formControlLabel}>Index Contributor</Typography>} />
+              </FormGroup>
+            </Grid>
+
+            <TabPanel value={value} index={0}>
+              <UnaffiliatedOrganizations
+                organization={affiliatedOrganizationsObject["unaffiliated"]}
+                unaffiliatedOpen= {unaffiliatedOpen}
+                searchCount={searchCount}
+                unaffiliated={unaffiliated}
+                unaffiliatedCount={unaffiliatedCount}
+                totalunaffiliatedCount={totalunaffiliatedCount}
+                checkboxValue={checkboxValue}
+              />
+              <Affiliated
+                organizations={affiliatedOrganizationsObject}
+                inputValue={inputValue}
+                classes={classes}
+                affiliation="affiliated"
+                affiliatedOpen={affiliatedOpen}
+                organizationData={organizationData}
+                affiliatedSepOpen= {affiliatedSepOpen}
+                searchCount={searchCount}
+                affiliatedCount={affiliatedCount}
+                totalaffiliatedCount={totalaffiliatedCount}
+                setAfflnSepOpen={setAfflnSepOpen}
+                checkboxValue={checkboxValue}
+              />
+            </TabPanel>
+            <TabPanel value={value} index={1}>
+              <UnaffiliatedOrganizations
+                organization={affiliatedOrganizationsObject["unaffiliated"]}
+                unaffiliatedOpen= {unaffiliatedOpen}
+                searchCount={searchCount}
+                unaffiliatedCount={unaffiliatedCount}
+                totalunaffiliatedCount={totalunaffiliatedCount}
+                checkboxValue={checkboxValue}
+              />
+            </TabPanel>
+            <TabPanel value={value} index={2}>
+              <Affiliated
+                organizations={affiliatedOrganizationsObject}
+                inputValue={inputValue}
+                classes={classes}
+                affiliation="affiliated"
+                organizationData={organizationData}
+                affiliatedSepOpen= {affiliatedSepOpen}
+                searchCount={searchCount}
+                affiliatedCount={affiliatedCount}
+                totalaffiliatedCount={totalaffiliatedCount}
+                setAfflnSepOpen={setAfflnSepOpen}
+                checkboxValue={checkboxValue}
+              />
+            </TabPanel>
+          </Grid>
+        </Container>
+
+      </Box>
+
+      <Box className='containerWhite'>
+        <Container>
+          <GetStartedCard
+            headerTitle='Want to add your organization?'
+            buttonText='Tag your project'
+            buttonHref='/taggenerator'
+          />
+        </Container>
+      </Box>
     </Box>
+
+
   );
 }
 
-const Affiliation = ({ organizations, inputValue, classes, affiliation }) => {
-  if (!organizations && !inputValue) {
-    return <h3 className={classes.loaders}>Loading...</h3>;
-  } else if (!organizations && inputValue) {
-    return <h3 className={classes.loaders}>No Results</h3>;
-  } else {
-    if (affiliation === "unaffiliated") {
-      return <UnaffiliatedOrganizations unAffiliatedOrgs={organizations} />;
-    } else {
-      return <AffiliatedOrganizations organizations={organizations} />;
-    }
-  }
-};
+
+
+const useStyles = makeStyles(theme => ({
+  root:
+  {
+
+    "& .MuiAutocomplete-inputRoot": {
+      paddingRight: '14px',
+
+    },
+
+  },
+  icon: {
+    backgroundColor: theme.palette.secondary.main,
+    borderBottomRightRadius: '4px',
+    borderTopRightRadius: '4px',
+    color: theme.palette.text.secondary,
+    height: '56px',
+    marginRight: '-14px',
+    width: '51px',
+  },
+  input: {
+    width: '945px',
+    height: '64px',
+    borderRadius: '7px',
+    fontSize: '1.5rem',
+    paddingBottom: '4em',
+    paddingTop: '15px',
+  },
+  "& .MuiSvgIcon-root": {
+    root: {
+      width: 168,
+      height: 168,
+    },
+  },
+
+
+}));
+
 
 const TopCallToAction = ({
-  heading,
-  tagline,
   input,
   options,
   inputPlaceholder,
   setInputValue,
 }) => {
-  const classes = useStyle();
+  const classes = useStyles();
+  const [selectedValue, changeInputVal] = useState(input)
+
+  const handleClick = () => {
+    setInputValue(selectedValue)
+  };
+
   return (
-    <div style={{ display: "grid", placeItems: "center" }}>
-      <h1 style={{ marginBottom: 0 }}>{heading}</h1>
-      <div
-        style={{
-          color: "white",
-          textAlign: "center",
-          margin: "3rem 0 ",
-          fontSize: "1.5rem",
-        }}
-      >
-        {tagline}
-      </div>
+    <Grid>
       <Autocomplete
         id="free-solo"
         freeSolo
         inputValue={input}
-        onInputChange={(e, newValue) => setInputValue(() => newValue)}
+        onInputChange={(e, newValue) => changeInputVal(newValue)}
         options={options}
-        className={classes.input}
+        className={classes.root}
+        disableClearable
+        forcePopupIcon={false}
         renderInput={(params) => (
           <TextField
             {...params}
             placeholder={inputPlaceholder}
+            className={classes.input}
             variant="outlined"
             InputProps={{
               ...params.InputProps,
               type: "search",
-              startAdornment: (
-                <InputAdornment position="start">
-                  <SearchRoundedIcon />
+
+              endAdornment: (
+                <InputAdornment position='end'>
+                  <SearchRoundedIcon onClick={handleClick} className={classes.icon} />
+
                 </InputAdornment>
               ),
             }}
           />
         )}
       />
-    </div>
+
+    </Grid>
   );
 };
