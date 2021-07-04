8ea2038c03986c7e321ba27738ac41c7d851c880
Kevin Ashworth <kevinashworth@yahoo.com>
2021-07-04 09:50:25 -0700

    Update App.test - smoke test, looks for 'Civic Tech Index'. Solve console error thingy about window.scrollTo

--- src/App.test.js
+++ src/App.test.js
@@ -2,8 +2,8 @@ import React from 'react';
 import { render } from '@testing-library/react';
 import App from './App';
 
-test('renders learn react link', () => {
-  const { getByText } = render(<App />);
-  const linkElement = getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+test('renders Civic Tech Index', () => {
+  const { getAllByText } = render(<App />);
+  const ctiElements = getAllByText(/Civic Tech Index/);
+  expect(ctiElements[0]).toBeInTheDocument();
 });
